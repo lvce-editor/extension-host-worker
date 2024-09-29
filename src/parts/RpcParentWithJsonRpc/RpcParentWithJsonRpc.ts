a5473fa465563f2bb743740806883b4ea3cbcc43
Le Vivilet <levivlet@pm.me>
2024-09-29 21:38:49 +0200

    remove unused code

--- src/parts/RpcParentWithJsonRpc/RpcParentWithJsonRpc.ts
+++ src/parts/RpcParentWithJsonRpc/RpcParentWithJsonRpc.ts
@@ -1,13 +1,24 @@
 import * as Assert from '../Assert/Assert.ts'
 import * as Callback from '../Callback/Callback.ts'
-import * as HandleJsonRpcMessage from '../HandleJsonRpcMessage/HandleJsonRpcMessage.ts'
 import * as JsonRpc from '../JsonRpc/JsonRpc.ts'
+
+const preparePrettyError = (error) => {
+  return error
+}
+
+const logError = (error) => {
+  // handled by renderer worker
+}
+
+const requiresSocket = () => {
+  return false
+}
 
 export const create = ({ ipc, execute }) => {
   Assert.object(ipc)
   Assert.fn(execute)
   const handleMessage = async (message) => {
-    return HandleJsonRpcMessage.handleJsonRpcMessage(ipc, message, execute, Callback.resolve)
+    return JsonRpc.handleJsonRpcMessage(ipc, message, execute, Callback.resolve, preparePrettyError, logError, requiresSocket)
   }
   ipc.onmessage = handleMessage
   return {
