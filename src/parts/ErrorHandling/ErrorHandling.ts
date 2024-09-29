ff49e748edbd17e98405226d6af431715b451a2c
Le Vivilet <levivlet@pm.me>
2024-09-29 21:33:01 +0200

    remove unused

--- src/parts/ErrorHandling/ErrorHandling.ts
+++ src/parts/ErrorHandling/ErrorHandling.ts
@@ -1,27 +1,5 @@
-import * as PrettyError from '../PrettyError/PrettyError.ts'
-
-const state = {
-  /**
-   * @type {string[]}
-   */
-  seenWarnings: [],
-}
-
-export const logError = async (error) => {
-  const prettyError = await PrettyError.prepare(error)
-  const prettyErrorString = PrettyError.print(prettyError)
-  console.error(prettyErrorString)
-  return prettyError
-}
-
 const handleError = async (error) => {
-  try {
-    await logError(error)
-  } catch (otherError) {
-    console.warn('ErrorHandling error')
-    console.warn(otherError)
-    console.error(error)
-  }
+  console.error(error)
 }
 
 /**
