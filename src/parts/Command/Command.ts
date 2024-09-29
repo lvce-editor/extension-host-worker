739a3707748be23fcca4affae1a6fa3b02c3bcf1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-29 21:45:53 +0200

    feature: simplify commandmap (#8)

--- src/parts/Command/Command.ts
+++ src/parts/Command/Command.ts
@@ -1,13 +1,17 @@
-export const state = {
-  getFn() {},
+import { CommandNotFoundError } from '../CommandNotFoundError/CommandNotFoundError.ts'
+
+const state = {}
+
+export const register = (commandMap) => {
+  Object.assign(state, commandMap)
 }
 
 export const execute = (method, ...params) => {
   // @ts-ignore
-  const fn = state.getFn(method)
+  const fn = state[method]
   // @ts-ignore
   if (!fn) {
-    throw new Error(`command not found ${method}`)
+    throw new CommandNotFoundError(method)
   }
   // @ts-ignore
   return fn(...params)
