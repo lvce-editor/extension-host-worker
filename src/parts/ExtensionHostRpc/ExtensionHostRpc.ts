2cdc42b5b1f785d8db9834de4cdc9e9b5ba4abba
Le Vivilet <levivlet@pm.me>
2024-10-04 15:30:07 +0200

    create rpc

--- src/parts/ExtensionHostRpc/ExtensionHostRpc.ts
+++ src/parts/ExtensionHostRpc/ExtensionHostRpc.ts
@@ -7,6 +7,7 @@ import * as IpcParentType from '../IpcParentType/IpcParentType.ts'
 import * as RpcParent from '../RpcParent/RpcParent.ts'
 import * as RpcParentType from '../RpcParentType/RpcParentType.ts'
 import { VError } from '../VError/VError.ts'
+import * as GetOrCreateRpcWithId from '../GetOrCreateRpcWithId/GetOrCreateRpcWithId.ts'
 
 const defaultExecute = () => {
   throw new Error('not implemented')
@@ -16,8 +17,7 @@ export const createRpc = async ({ id, url, name, execute = defaultExecute, conte
   try {
     if (id) {
       Assert.string(id)
-      const rpc = await CreateRpcWithId.createRpcWithId({ id, execute })
-      return rpc
+      return GetOrCreateRpcWithId.createRpcWithId({ id, execute })
     }
 
     // deprecated
--- src/parts/GetOrCreateRpcWithId/GetOrCreateRpcWithId.ts
+++ src/parts/GetOrCreateRpcWithId/GetOrCreateRpcWithId.ts
@@ -1,37 +1,11 @@
 import * as Assert from '../Assert/Assert.ts'
-import * as ExtensionHostRpcState from '../ExtensionHostRpcState/ExtensionHostRpcState.ts'
-import * as ExtensionHostSubWorkerUrl from '../ExtensionHostSubWorkerUrl/ExtensionHostSubWorkerUrl.ts'
-import * as IpcParent from '../IpcParent/IpcParent.ts'
-import * as IpcParentType from '../IpcParentType/IpcParentType.ts'
-import * as RpcParent from '../RpcParent/RpcParent.ts'
-import * as RpcParentType from '../RpcParentType/RpcParentType.ts'
+import * as CreateRpcWithId from '../CreateRpcWithId/CreateRpcWithId.ts'
 import * as RpcState from '../RpcState/RpcState.ts'
-
-const createRpc = async (id: string) => {
-  const fn = RpcState.acquire(id)
-  const info = ExtensionHostRpcState.get(id)
-  if (!info) {
-    throw new Error(`rpc with id ${id} not found`)
-  }
-  const ipc = await IpcParent.create({
-    method: IpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
-    url: ExtensionHostSubWorkerUrl.extensionHostSubWorkerUrl,
-    name: info.name,
-  })
-  const newRpc = await RpcParent.create({
-    ipc,
-    method: RpcParentType.JsonRpc,
-    execute: fn,
-  })
-  await newRpc.invoke('LoadFile.loadFile', info.url)
-  RpcState.set(id, newRpc)
-  return newRpc
-}
 
 const getOrCreateRpc = async (id: string) => {
   const rpc = RpcState.get(id)
   if (!rpc) {
-    RpcState.set(id, createRpc(id))
+    RpcState.set(id, CreateRpcWithId.createRpcWithId(id))
   }
   return RpcState.get(id)
 }
