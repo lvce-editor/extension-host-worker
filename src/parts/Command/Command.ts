62249251193f1d74ae93bbd0e85e11144fa0fcca
Le Vivilet <levivlet@pm.me>
2024-10-24 23:04:37 +0200

    feature: use command npm module

--- src/parts/Command/Command.ts
+++ src/parts/Command/Command.ts
@@ -1,18 +1,1 @@
-import { CommandNotFoundError } from '../CommandNotFoundError/CommandNotFoundError.ts'
-
-const state = {}
-
-export const register = (commandMap) => {
-  Object.assign(state, commandMap)
-}
-
-export const execute = (method, ...params) => {
-  // @ts-ignore
-  const fn = state[method]
-  // @ts-ignore
-  if (!fn) {
-    throw new CommandNotFoundError(method)
-  }
-  // @ts-ignore
-  return fn(...params)
-}
+export * from '@lvce-editor/command'
