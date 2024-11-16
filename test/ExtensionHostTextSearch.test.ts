f24ff6f6d5ccb2842280a44896d256c7ccf35945
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-16 17:50:05 +0100

    feature: improve error message when no text search provider is found (#29)

--- test/ExtensionHostTextSearch.test.ts
+++ test/ExtensionHostTextSearch.test.ts
@@ -8,7 +8,7 @@ beforeEach(() => {
 test('registerTextSearchProvider - no argument provided', () => {
   // @ts-expect-error
   expect(() => ExtensionHostTextSearch.registerTextSearchProvider()).toThrow(
-    new Error('Failed to register text search provider: textSearchProvider is not defined'),
+    new Error('Failed to register text search provider: textSearchProvider is not defined')
   )
 })
 
@@ -16,7 +16,7 @@ test('registerTextSearchProvider - missing scheme', () => {
   expect(() =>
     ExtensionHostTextSearch.registerTextSearchProvider({
       provideTextSearchResults(query) {},
-    }),
+    })
   ).toThrow(new Error('Failed to register text search provider: textSearchProvider is missing scheme'))
 })
 
@@ -24,13 +24,13 @@ test('registerTextSearchProvider - missing provideTextSearchResults function', (
   expect(() =>
     ExtensionHostTextSearch.registerTextSearchProvider({
       provideTextSearchResults(query) {},
-    }),
+    })
   ).toThrow(new Error('Failed to register text search provider: textSearchProvider is missing scheme'))
 })
 
 test('executeTextSearchProvider - no provider found', async () => {
   await expect(ExtensionHostTextSearch.executeTextSearchProvider('xyz', 'abc')).rejects.toThrow(
-    new Error('Failed to execute text search provider: no text search provider for xyz found'),
+    new Error('Failed to execute text search provider: No text search provider for xyz found')
   )
 })
 
@@ -42,6 +42,6 @@ test('executeTextSearchProvider - error', async () => {
     },
   })
   await expect(ExtensionHostTextSearch.executeTextSearchProvider('xyz', 'abc')).rejects.toThrow(
-    new Error('Failed to execute text search provider: TypeError: x is not a function'),
+    new Error('Failed to execute text search provider: TypeError: x is not a function')
   )
 })
