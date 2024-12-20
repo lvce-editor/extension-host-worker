c6abe91f58f553e603659a33bd9fb02e6d51a5a6
Le Vivilet <levivlet@pm.me>
2024-12-20 18:52:05 +0100

    feature: add getWebViewInfo command

--- src/parts/ExtensionHostWebViewState/ExtensionHostWebViewState.ts
+++ src/parts/ExtensionHostWebViewState/ExtensionHostWebViewState.ts
@@ -1,19 +1,19 @@
 const webViews = Object.create(null)
 const webViewProviders = Object.create(null)
 
-export const getProvider = (providerId) => {
+export const getProvider = (providerId: string): any => {
   return webViewProviders[providerId]
 }
 
-export const setProvider = (providerId, provider) => {
+export const setProvider = (providerId: string, provider: any): void => {
   webViewProviders[providerId] = provider
 }
 
-export const getWebView = (id) => {
+export const getWebView = (id: string): any => {
   return webViews[id]
 }
 
-export const setWebView = (id, webView) => {
+export const setWebView = (id: string, webView: any): void => {
   webViews[id] = webView
 }
 
