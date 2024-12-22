385cc93439c00ebaadecac1fe723882476b3a09c
Le Vivilet <levivlet@pm.me>
2024-12-22 13:29:07 +0100

    feature: handle more edge cases for unhandled rejections

--- test/HandleUnhandledRejection.test.ts
+++ test/HandleUnhandledRejection.test.ts
@@ -30,3 +30,46 @@ test('handleUnhandledRejection - non-error object', () => {
   expect(console.error).toHaveBeenCalledTimes(1)
   expect(console.error).toHaveBeenCalledWith('[extension host worker] Unhandled Rejection: custom error')
 })
+
+test('handleUnhandledRejection - null', () => {
+  HandleUnhandledRejection.handleUnhandledRejection(null)
+  expect(console.error).toHaveBeenCalledTimes(1)
+  expect(console.error).toHaveBeenCalledWith('[extension host worker] Unhandled Rejection: null')
+})
+
+test('handleUnhandledRejection - undefined', () => {
+  HandleUnhandledRejection.handleUnhandledRejection(undefined)
+  expect(console.error).toHaveBeenCalledTimes(1)
+  expect(console.error).toHaveBeenCalledWith('[extension host worker] Unhandled Rejection: undefined')
+})
+
+test('handleUnhandledRejection - number', () => {
+  HandleUnhandledRejection.handleUnhandledRejection(42)
+  expect(console.error).toHaveBeenCalledTimes(1)
+  expect(console.error).toHaveBeenCalledWith('[extension host worker] Unhandled Rejection: 42')
+})
+
+test('handleUnhandledRejection - boolean', () => {
+  HandleUnhandledRejection.handleUnhandledRejection(true)
+  expect(console.error).toHaveBeenCalledTimes(1)
+  expect(console.error).toHaveBeenCalledWith('[extension host worker] Unhandled Rejection: true')
+})
+
+test('handleUnhandledRejection - string', () => {
+  HandleUnhandledRejection.handleUnhandledRejection('error message')
+  expect(console.error).toHaveBeenCalledTimes(1)
+  expect(console.error).toHaveBeenCalledWith('[extension host worker] Unhandled Rejection: error message')
+})
+
+test('handleUnhandledRejection - array', () => {
+  HandleUnhandledRejection.handleUnhandledRejection([1, 2, 3])
+  expect(console.error).toHaveBeenCalledTimes(1)
+  expect(console.error).toHaveBeenCalledWith('[extension host worker] Unhandled Rejection: 1,2,3')
+})
+
+test('handleUnhandledRejection - symbol', () => {
+  const sym = Symbol('test')
+  HandleUnhandledRejection.handleUnhandledRejection(sym)
+  expect(console.error).toHaveBeenCalledTimes(1)
+  expect(console.error).toHaveBeenCalledWith('[extension host worker] Unhandled Rejection: Symbol(test)')
+})
