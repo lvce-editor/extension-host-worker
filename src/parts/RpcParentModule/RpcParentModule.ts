7be073723eaf76a487b897e9b684b51a1b0cd568
Le Vivilet <levivlet@pm.me>
2024-12-28 08:53:33 +0100

    feature: simplify rpc imports

--- src/parts/RpcParentModule/RpcParentModule.ts
+++ src/parts/RpcParentModule/RpcParentModule.ts
@@ -1,9 +1,10 @@
 import * as RpcParentType from '../RpcParentType/RpcParentType.ts'
+import * as RpcParentWithJsonRpc from '../RpcParentWithJsonRpc/RpcParentWithJsonRpc.ts'
 
 export const getModule = (method) => {
   switch (method) {
     case RpcParentType.JsonRpc:
-      return import('../RpcParentWithJsonRpc/RpcParentWithJsonRpc.ts')
+      return RpcParentWithJsonRpc
     default:
       throw new Error('unexpected rpc type')
   }
