e5f22d99830951cc749a913e077948f5f37094a4
Le Vivilet <levivlet@pm.me>
2024-12-28 16:31:25 +0100

    feature: use npm package for electron ipc

--- src/parts/IpcParentWithElectronMessagePort/IpcParentWithElectronMessagePort.ts
+++ src/parts/IpcParentWithElectronMessagePort/IpcParentWithElectronMessagePort.ts
@@ -1,39 +1,22 @@
-import * as GetPortTuple from '../GetPortTuple/GetPortTuple.ts'
+import { IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug } from '@lvce-editor/ipc'
 import * as SendMessagePortToElectron from '../SendMessagePortToElectron/SendMessagePortToElectron.ts'
 
-const getPort = async (type) => {
-  const { port1, port2 } = GetPortTuple.getPortTuple()
+const sendPort = async ({ port }: { port: MessagePort }) => {
   await SendMessagePortToElectron.sendMessagePortToElectron(
-    port1,
-    'HandleMessagePortForExtensionHostHelperProcess.handleMessagePortForExtensionHostHelperProcess',
+    port,
+    'HandleMessagePortForExtensionHostHelperProcess.handleMessagePortForExtensionHostHelperProcess'
   )
-  return port2
 }
 
 export const create = async ({ type }) => {
-  const port = await getPort(type)
+  const port = await IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug.create({
+    url: '',
+    name: '',
+    sendPort,
+  })
   return port
 }
 
 export const wrap = (port) => {
-  let handleMessage
-  return {
-    get onmessage() {
-      return handleMessage
-    },
-    set onmessage(listener) {
-      let handleMessage
-      if (listener) {
-        handleMessage = (event) => {
-          listener(event.data)
-        }
-      } else {
-        handleMessage = null
-      }
-      port.onmessage = handleMessage
-    },
-    send(message) {
-      port.postMessage(message)
-    },
-  }
+  return IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug.wrap(port)
 }
