5ff1255d4a40b68467577ac3e6fbabc4f968d8a8
Le Vivilet <levivlet@pm.me>
2024-12-28 18:57:03 +0100

    move code

---
+++ packages/extension-host-worker/src/parts/parts/GetConfiguredIframeWorkerUrl/GetConfiguredIframeWorkerUrl.ts
@@ -0,0 +1,15 @@
+import * as IframeWorkerUrl from '../IframeWorkerUrl/IframeWorkerUrl.ts'
+import * as IsProduction from '../IsProduction/IsProduction.ts'
+import * as Preferences from '../Preferences/Preferences.ts'
+
+export const getConfiguredIframeWorkerUrl = () => {
+  let configuredWorkerUrl = Preferences.get('develop.iframeWorkerPath') || ''
+  if (configuredWorkerUrl) {
+    configuredWorkerUrl = '/remote' + configuredWorkerUrl
+  }
+  configuredWorkerUrl = configuredWorkerUrl || IframeWorkerUrl.iframeWorkerUrl
+  if (IsProduction.isProduction) {
+    configuredWorkerUrl = IframeWorkerUrl.iframeWorkerUrl
+  }
+  return configuredWorkerUrl
+}
