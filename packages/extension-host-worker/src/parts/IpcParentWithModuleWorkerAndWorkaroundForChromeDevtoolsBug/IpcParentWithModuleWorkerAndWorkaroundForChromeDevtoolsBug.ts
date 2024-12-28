44eebf0dca31ef857903d4f88e34ff4bec03b4bc
Le Vivilet <levivlet@pm.me>
2024-12-28 19:16:21 +0100

    feature: use npm package for module worker ipc

--- packages/extension-host-worker/src/parts/IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug/IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug.ts
+++ packages/extension-host-worker/src/parts/IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug/IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug.ts
@@ -1,18 +1,25 @@
+import { IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug } from '@lvce-editor/ipc'
 import * as Assert from '../Assert/Assert.ts'
-import * as GetPortTuple from '../GetPortTuple/GetPortTuple.ts'
 import * as RendererWorkerIpcParentType from '../RendererWorkerIpcParentType/RendererWorkerIpcParentType.ts'
 import * as Rpc from '../Rpc/Rpc.ts'
 
-export const create = async ({ url, name }) => {
-  Assert.string(url)
-  Assert.string(name)
-  const { port1, port2 } = GetPortTuple.getPortTuple()
+const sendPort = async ({ url, name, port }: { url: string; name: string; port: MessagePort }): Promise<void> => {
   await Rpc.invokeAndTransfer('IpcParent.create', {
     method: RendererWorkerIpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
     url,
     name,
     raw: true,
-    port: port1,
+    port,
+  })
+}
+
+export const create = async ({ url, name }) => {
+  Assert.string(url)
+  Assert.string(name)
+  const port2 = IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug.create({
+    url,
+    name,
+    sendPort,
   })
   return port2
 }
