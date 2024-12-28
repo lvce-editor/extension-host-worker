a71d348d117f5fcb659728dca0733f64c739f35b
Le Vivilet <levivlet@pm.me>
2024-12-28 17:17:00 +0100

    move code

--- src/parts/LaunchIframeWorker/LaunchIframeWorker.ts
+++ src/parts/LaunchIframeWorker/LaunchIframeWorker.ts
@@ -1,25 +1,11 @@
+import * as GetConfiguredIframeWorkerUrl from '../GetConfiguredIframeWorkerUrl/GetConfiguredIframeWorkerUrl.ts'
 import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
 import * as Id from '../Id/Id.js'
 import * as IpcParent from '../IpcParent/IpcParent.ts'
 import * as IpcParentType from '../IpcParentType/IpcParentType.ts'
-import * as IsProduction from '../IsProduction/IsProduction.ts'
-import * as Preferences from '../Preferences/Preferences.ts'
-import * as IframeWorkerUrl from '../IframeWorkerUrl/IframeWorkerUrl.ts'
-
-const getConfiguredWorkerUrl = () => {
-  let configuredWorkerUrl = Preferences.get('develop.iframeWorkerPath') || ''
-  if (configuredWorkerUrl) {
-    configuredWorkerUrl = '/remote' + configuredWorkerUrl
-  }
-  configuredWorkerUrl = configuredWorkerUrl || IframeWorkerUrl.iframeWorkerUrl
-  if (IsProduction.isProduction) {
-    configuredWorkerUrl = IframeWorkerUrl.iframeWorkerUrl
-  }
-  return configuredWorkerUrl
-}
 
 export const launchIframeWorker = async () => {
-  const configuredWorkerUrl = getConfiguredWorkerUrl()
+  const configuredWorkerUrl = GetConfiguredIframeWorkerUrl.getConfiguredIframeWorkerUrl()
   const name = 'Iframe Worker'
   const id = Id.create()
   let ipc = await IpcParent.create({
