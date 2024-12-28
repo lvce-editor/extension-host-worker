01baf57c8dcc373f1ece42eba92b71026c1a69c7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-28 19:17:20 +0100

    feature: update postinstall script (#101)

--- packages/server/src/postinstall.js
+++ packages/server/src/postinstall.js
@@ -1,4 +1,4 @@
-import { readdir, readFile } from 'node:fs/promises'
+import { readFile, writeFile } from 'node:fs/promises'
 import { dirname, join } from 'node:path'
 import { fileURLToPath, pathToFileURL } from 'node:url'
 
@@ -13,27 +13,23 @@ export const getRemoteUrl = (path) => {
 
 const nodeModulesPath = join(root, 'packages', 'server', 'node_modules')
 
-const aboutWorkerPath = join(root, '.tmp', 'dist', 'dist', 'aboutWorkerMain.js')
+const extensionHostWorkerPath = join(root, '.tmp', 'dist', 'dist', 'extensionHostWorkerMain.js')
 
-const serverStaticPath = join(nodeModulesPath, '@lvce-editor', 'static-server', 'static')
+const staticPath = join(nodeModulesPath, '@lvce-editor', 'static-server', 'static')
+const indexHtmlPath = join(staticPath, 'index.html')
 
-const RE_COMMIT_HASH = /^[a-z\d]+$/
-const isCommitHash = (dirent) => {
-  return dirent.length === 7 && dirent.match(RE_COMMIT_HASH)
+const indexHtmlContent = await readFile(indexHtmlPath, 'utf8')
+
+const remoteUrl = getRemoteUrl(extensionHostWorkerPath)
+
+const config = {
+  'develop.extensionHostWorkerPath': remoteUrl,
 }
+const stringifiedConfig = JSON.stringify(config, null, 2)
+const newContent = indexHtmlContent.replace(
+  '</title>',
+  `</title>
+  <script type="application/json" id="Config">${stringifiedConfig}</script>`
+)
 
-const dirents = await readdir(serverStaticPath)
-const commitHash = dirents.find(isCommitHash) || ''
-const rendererWorkerMainPath = join(serverStaticPath, commitHash, 'packages', 'renderer-worker', 'dist', 'rendererWorkerMain.js')
-
-const content = await readFile(rendererWorkerMainPath, 'utf-8')
-// TODO
-// @ts-ignore
-const remoteUrl = getRemoteUrl(aboutWorkerPath)
-if (!content.includes('// const aboutWorkerUrl = ')) {
-  // const occurrence = `const aboutWorkerUrl = \`\${assetDir}/packages/about-view-worker/dist/aboutWorkerMain.js\``
-  // const replacement = `// const aboutWorkerUrl = \`\${assetDir}/packages/about-view-worker/dist/aboutWorkerMain.js\`
-  // const aboutWorkerUrl = \`${remoteUrl}\``
-  // const newContent = content.replace(occurrence, replacement)
-  // await writeFile(rendererWorkerMainPath, newContent)
-}
+await writeFile(indexHtmlPath, newContent)
