9e4c6c62a25a8dac52f4dab9ce6d6fd625adccdc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-29 11:58:50 +0100

    feature: add another webview e2e test (#132)

---
+++ packages/e2e/src/sample.webview-provider-message.js
@@ -0,0 +1,21 @@
+export const name = 'sample.webview-provider-message'
+
+export const skip = true
+
+export const test = async ({ Extension, Main, FileSystem, WebView }) => {
+  // arrange
+  await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.xyz`, `a`)
+
+  // act
+  await Main.openUri(`${tmpDir}/test.xyz`)
+
+  // assert
+  const webView = await WebView.fromId('xyz')
+  // @ts-ignore
+  const body = webView.locator('body')
+
+  // TODO
+  // await expect(body).toHaveText('124')
+}
