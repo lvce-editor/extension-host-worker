0854bd4e4169933aaaa629496f22fee21890d5de
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-29 10:20:21 +0100

    Discard changes to packages/extension-host-worker/src/parts/IpcParent/IpcParent.ts

--- packages/extension-host-worker/src/parts/IpcParent/IpcParent.ts
+++ packages/extension-host-worker/src/parts/IpcParent/IpcParent.ts
@@ -1,14 +1,15 @@
-import { Rpc } from '@lvce-editor/rpc'
 import * as IpcParentModule from '../IpcParentModule/IpcParentModule.ts'
 import { VError } from '../VError/VError.ts'
 
-export const create = async ({ method, ...options }): Promise<Rpc> => {
+export const create = async ({ method, ...options }) => {
   try {
     const module = IpcParentModule.getModule(method)
     // @ts-ignore
-    const rpc = await module.create(options)
-    return rpc
+    const rawIpc = await module.create(options)
+    // @ts-ignore
+    const ipc = module.wrap(rawIpc)
+    return ipc
   } catch (error) {
-    throw new VError(error, `Failed to create rpc`)
+    throw new VError(error, `Failed to create ipc`)
   }
 }
