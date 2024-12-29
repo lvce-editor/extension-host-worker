6aeffbd6888d5dd0c2633319daf026f5398be618
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-29 10:20:06 +0100

    Discard changes to packages/extension-host-worker/src/parts/HandleMessagePort/HandleMessagePort.ts

--- packages/extension-host-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
+++ packages/extension-host-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
@@ -1,8 +1,11 @@
-// @ts-ignore
-import { MessagePortRpcClient } from '@lvce-editor/rpc'
+import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
+import * as IpcChildModule from '../IpcChildModule/IpcChildModule.ts'
+import * as IpcChildType from '../IpcChildType/IpcChildType.ts'
 
-export const handleMessagePort = async (port: MessagePort) => {
-  await MessagePortRpcClient.create({
-    messagePort: port,
-  })
+export const handleMessagePort = (port: MessagePort) => {
+  const module = IpcChildModule.getModule(IpcChildType.MessagePort)
+  // @ts-ignore
+  const ipc = module.wrap(port)
+  HandleIpc.handleIpc(ipc)
+  ipc.send('ready')
 }
