5b7f11f1379a0f5fb96b49cd1a2a7fd97dc119ef
Le Vivilet <levivlet@pm.me>
2024-12-29 13:07:22 +0100

    prefix

--- packages/extension-host-worker/src/parts/LaunchIframeWorker/LaunchIframeWorker.ts
+++ packages/extension-host-worker/src/parts/LaunchIframeWorker/LaunchIframeWorker.ts
@@ -5,15 +5,16 @@ import * as IpcParentType from '../IpcParentType/IpcParentType.ts'
 import * as Rpc from '../Rpc/Rpc.ts'
 
 const commandMap = {
-  compatExtensionHostWorkerInvoke: (...args) => Rpc.invoke('WebView.compatExtensionHostWorkerInvoke', ...args),
-  compatExtensionHostWorkerInvokeAndTransfer: (...args) => Rpc.invokeAndTransfer('WebView.compatExtensionHostWorkerInvokeAndTransfer', ...args),
-  compatRendererProcessInvoke: (...args) => Rpc.invoke('WebView.compatRendererProcessInvoke', ...args),
-  compatRendererProcessInvokeAndTransfer: (...args) => Rpc.invokeAndTransfer('WebView.compatRendererProcessInvokeAndTransfer', ...args),
-  compatSharedProcessInvoke: (...args) => Rpc.invoke('WebView.compatSharedProcessInvoke', ...args),
-  getSavedState: (...args) => Rpc.invoke('WebView.WebView', ...args),
-  getWebViewInfo: (...args) => Rpc.invoke('WebView.getWebViewInfo', ...args),
-  getWebViews: (...args) => Rpc.invoke('WebView.getWebViews', ...args),
-  setPort: (...args) => Rpc.invoke('WebView.setPort', ...args),
+  'WebView.compatExtensionHostWorkerInvoke': (...args) => Rpc.invoke('WebView.compatExtensionHostWorkerInvoke', ...args),
+  'WebView.compatExtensionHostWorkerInvokeAndTransfer': (...args) =>
+    Rpc.invokeAndTransfer('WebView.compatExtensionHostWorkerInvokeAndTransfer', ...args),
+  'WebView.compatRendererProcessInvoke': (...args) => Rpc.invoke('WebView.compatRendererProcessInvoke', ...args),
+  'WebView.compatRendererProcessInvokeAndTransfer': (...args) => Rpc.invokeAndTransfer('WebView.compatRendererProcessInvokeAndTransfer', ...args),
+  'WebView.compatSharedProcessInvoke': (...args) => Rpc.invoke('WebView.compatSharedProcessInvoke', ...args),
+  'WebView.getSavedState': (...args) => Rpc.invoke('WebView.WebView', ...args),
+  'WebView.getWebViewInfo': (...args) => Rpc.invoke('WebView.getWebViewInfo', ...args),
+  'WebView.getWebViews': (...args) => Rpc.invoke('WebView.getWebViews', ...args),
+  'WebView.setPort': (...args) => Rpc.invoke('WebView.setPort', ...args),
 }
 
 export const launchIframeWorker = async () => {
