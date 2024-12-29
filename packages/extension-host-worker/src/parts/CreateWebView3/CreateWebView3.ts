9a3b5d0cfa65d998d2be83e6c8b3c22acd2645be
Le Vivilet <levivlet@pm.me>
2024-12-29 12:47:30 +0100

    send assetDir

--- packages/extension-host-worker/src/parts/CreateWebView3/CreateWebView3.ts
+++ packages/extension-host-worker/src/parts/CreateWebView3/CreateWebView3.ts
@@ -5,16 +5,19 @@ export const createWebView3 = async ({
   uri,
   isGitpod,
   platform,
+  assetDir,
 }: {
   id: number
   uri: string
   isGitpod: boolean
   platform: number
+  assetDir: string
 }): Promise<void> => {
   await IframeWorker.invoke('WebView.create3', {
     id,
     uri,
     isGitpod,
     platform,
+    assetDir,
   })
 }
