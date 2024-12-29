85606aa77db5720442136bc4875db1cd4c50f365
Le Vivilet <levivlet@pm.me>
2024-12-29 10:21:14 +0100

    undo

--- packages/extension-host-worker/src/parts/IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug/IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug.ts
+++ packages/extension-host-worker/src/parts/IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug/IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug.ts
@@ -1,11 +1,10 @@
-import { MessagePortRpcParent, type Rpc } from '@lvce-editor/rpc'
+import { IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug } from '@lvce-editor/ipc'
 import * as Assert from '../Assert/Assert.ts'
-import * as GetPortTuple from '../GetPortTuple/GetPortTuple.ts'
 import * as RendererWorkerIpcParentType from '../RendererWorkerIpcParentType/RendererWorkerIpcParentType.ts'
-import * as ParentRpc from '../Rpc/Rpc.ts'
+import * as Rpc from '../Rpc/Rpc.ts'
 
 const sendPort = async ({ url, name, port }: { url: string; name: string; port: MessagePort }): Promise<void> => {
-  await ParentRpc.invokeAndTransfer('IpcParent.create', {
+  await Rpc.invokeAndTransfer('IpcParent.create', {
     method: RendererWorkerIpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
     url,
     name,
@@ -14,15 +13,34 @@ const sendPort = async ({ url, name, port }: { url: string; name: string; port: 
   })
 }
 
-export const create = async ({ url, name }: { url: string; name: string }): Promise<Rpc> => {
+export const create = async ({ url, name }: { url: string; name: string }): Promise<MessagePort> => {
   Assert.string(url)
   Assert.string(name)
-  const { port1, port2 } = GetPortTuple.getPortTuple()
-  await sendPort({ url, name, port: port1 })
-  const rpc = await MessagePortRpcParent.create({
-    messagePort: port2,
-    isMessagePortOpen: true,
-    commandMap: {},
+  const port2 = IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug.create({
+    url,
+    name,
+    sendPort,
   })
-  return rpc
+  // @ts-ignore
+  return port2
+}
+
+export const wrap = (port: MessagePort) => {
+  // @ts-ignore
+  const wrapped = IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug.wrap(port)
+  return {
+    wrapped,
+    set onmessage(listener) {
+      this.wrapped.addEventListener('message', (event) => {
+        // @ts-ignore
+        listener(event.data)
+      })
+    },
+    send(message) {
+      this.wrapped.send(message)
+    },
+    sendAndTransfer(message, transfer) {
+      this.wrapped.sendAndTransfer(message, transfer)
+    },
+  }
 }
