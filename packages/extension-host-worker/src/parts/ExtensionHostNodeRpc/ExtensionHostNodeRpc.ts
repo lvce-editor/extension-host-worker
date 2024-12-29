dfbe8a0b678f9e06ffd5d59ea6f06e1a6f2e17e8
Le Vivilet <levivlet@pm.me>
2024-12-29 10:27:36 +0100

    tsc

--- packages/extension-host-worker/src/parts/ExtensionHostNodeRpc/ExtensionHostNodeRpc.ts
+++ packages/extension-host-worker/src/parts/ExtensionHostNodeRpc/ExtensionHostNodeRpc.ts
@@ -1,8 +1,6 @@
 import * as Assert from '../Assert/Assert.ts'
 import * as IpcParent from '../IpcParent/IpcParent.ts'
 import * as IpcParentType from '../IpcParentType/IpcParentType.ts'
-import * as RpcParent from '../RpcParent/RpcParent.ts'
-import * as RpcParentType from '../RpcParentType/RpcParentType.ts'
 import { VError } from '../VError/VError.ts'
 
 const defaultExecute = () => {
