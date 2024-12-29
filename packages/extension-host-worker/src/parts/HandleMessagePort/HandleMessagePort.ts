f2c838881d79c5125ef6c692363b7355b0c5862b
Le Vivilet <levivlet@pm.me>
2024-12-29 10:25:08 +0100

    tsc

--- packages/extension-host-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
+++ packages/extension-host-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
@@ -4,5 +4,6 @@ import { MessagePortRpcClient } from '@lvce-editor/rpc'
 export const handleMessagePort = async (port: MessagePort) => {
   await MessagePortRpcClient.create({
     messagePort: port,
+    commandMap: {},
   })
 }
