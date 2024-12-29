ee2fc156a1e0efca84d6ee09a67e0324194b7811
Le Vivilet <levivlet@pm.me>
2024-12-29 11:32:57 +0100

    feature: pass platform variable instead of using global variable

--- packages/extension-host-worker/test/GetUrlPrefix.test.ts
+++ packages/extension-host-worker/test/GetUrlPrefix.test.ts
@@ -1,34 +1,27 @@
-import { beforeEach, expect, jest, test } from '@jest/globals'
+import { expect, test } from '@jest/globals'
+import * as GetUrlPrefix from '../src/parts/GetUrlPrefix/GetUrlPrefix.ts'
 import * as PlatformType from '../src/parts/PlatformType/PlatformType.ts'
 
-beforeEach(() => {
-  jest.resetAllMocks()
-})
-
-jest.unstable_mockModule('../src/parts/Platform/Platform.ts', () => {
-  return {
-    platform: PlatformType.Web,
-  }
-})
-
-const GetUrlPrefix = await import('../src/parts/GetUrlPrefix/GetUrlPrefix.ts')
-
 test('http url', () => {
+  const platform = PlatformType.Web
   const extensionPath = 'http://localhost:3000'
-  expect(GetUrlPrefix.getUrlPrefix(extensionPath)).toBe('http://localhost:3000')
+  expect(GetUrlPrefix.getUrlPrefix(platform, extensionPath)).toBe('http://localhost:3000')
 })
 
 test('https', () => {
+  const platform = PlatformType.Web
   const extensionPath = 'https://example.com'
-  expect(GetUrlPrefix.getUrlPrefix(extensionPath)).toBe('https://example.com')
+  expect(GetUrlPrefix.getUrlPrefix(platform, extensionPath)).toBe('https://example.com')
 })
 
 test('web', () => {
+  const platform = PlatformType.Web
   const extensionPath = '/video-preview/dist/extension.js'
-  expect(GetUrlPrefix.getUrlPrefix(extensionPath)).toBe('/video-preview/dist/extension.js')
+  expect(GetUrlPrefix.getUrlPrefix(platform, extensionPath)).toBe('/video-preview/dist/extension.js')
 })
 
 test.skip('remote', () => {
+  const platform = PlatformType.Web
   const extensionPath = '/home/user/video-preview/dist/extension.js'
-  expect(GetUrlPrefix.getUrlPrefix(extensionPath)).toBe('/remote/home/user/video-preview/dist/extension.js')
+  expect(GetUrlPrefix.getUrlPrefix(platform, extensionPath)).toBe('/remote/home/user/video-preview/dist/extension.js')
 })
