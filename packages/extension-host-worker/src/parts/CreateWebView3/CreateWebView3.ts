4f8abb1c912084b94bd5b4dc53a3b13fd730c3a6
Le Vivilet <levivlet@pm.me>
2024-12-29 12:46:48 +0100

    forward infos

--- packages/extension-host-worker/src/parts/CreateWebView3/CreateWebView3.ts
+++ packages/extension-host-worker/src/parts/CreateWebView3/CreateWebView3.ts
@@ -1,51 +1,20 @@
-import { MessagePortRpcParent } from '@lvce-editor/rpc'
-import * as ExtensionHostWebViewState from '../ExtensionHostWebViewState/ExtensionHostWebViewState.ts'
 import * as IframeWorker from '../IframeWorker/IframeWorker.ts'
 
-export const createWebView3 = async (id: number, uri: string): Promise<void> => {
-  await IframeWorker.invoke('')
-  const provider = ExtensionHostWebViewState.getProvider(providerId)
-  if (!provider) {
-    throw new Error(`webview provider ${providerId} not found`)
-  }
-
-  // TODO cancel promise when webview is disposed before sending message
-  // TODO handle case when webview doesn't send ready message
-
-  const rpc = await MessagePortRpcParent.create({
-    messagePort: port,
-    isMessagePortOpen: false,
-    commandMap: provider.commands || {},
-  })
-
-  const outer = {
+export const createWebView3 = async ({
+  id,
+  uri,
+  isGitpod,
+  platform,
+}: {
+  id: number
+  uri: string
+  isGitpod: boolean
+  platform: number
+}): Promise<void> => {
+  await IframeWorker.invoke('WebView.create3', {
+    id,
     uri,
-    provider,
-    uid,
-    origin,
-    webView,
-    async invoke(method, ...params) {
-      return rpc.invoke(method, ...params)
-    },
-  }
-  // TODO allow creating multiple webviews per provider
-  ExtensionHostWebViewState.setWebView(providerId, outer)
-}
-
-export const disposeWebView = (id) => {
-  // TODO race condition
-  // const webView=webViews[id]
-}
-
-export const registerWebViewProvider = (provider) => {
-  ExtensionHostWebViewState.setProvider(provider.id, provider)
-}
-
-export const getWebViewInfo = (providerId: string) => {
-  const webView = ExtensionHostWebViewState.getWebView(providerId)
-  return {
-    uid: webView.uid,
-    origin: webView.origin,
-    uri: webView.uri,
-  }
+    isGitpod,
+    platform,
+  })
 }
