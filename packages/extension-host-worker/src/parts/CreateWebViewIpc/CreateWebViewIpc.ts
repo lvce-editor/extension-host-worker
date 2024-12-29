ea9eaf3d187e01512fad633bdd3ff26f1aa43293
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-29 10:11:26 +0100

    Discard changes to packages/extension-host-worker/src/parts/CreateWebViewIpc/CreateWebViewIpc.ts

--- packages/extension-host-worker/src/parts/CreateWebViewIpc/CreateWebViewIpc.ts
+++ packages/extension-host-worker/src/parts/CreateWebViewIpc/CreateWebViewIpc.ts
@@ -1,19 +1,21 @@
-import { MessagePortRpcParent } from '@lvce-editor/rpc'
+import { IpcParentWithMessagePort } from '@lvce-editor/ipc'
 import * as GetPortTuple from '../GetPortTuple/GetPortTuple.ts'
+import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
 import * as Rpc from '../Rpc/Rpc.ts'
 
 export const createWebViewIpc = async (webView: any): Promise<any> => {
   const { uid, origin } = webView
   const { port1, port2 } = GetPortTuple.getPortTuple()
-  const rpcPromise = MessagePortRpcParent.create({
+  const readyPromse = IpcParentWithMessagePort.create({
     messagePort: port2,
     isMessagePortOpen: false,
-    commandMap: {},
   })
   const portType = 'test'
   await Rpc.invokeAndTransfer('WebView.setPort', uid, port1, origin, portType)
+  await readyPromse
+  const ipc = IpcParentWithMessagePort.wrap(port2)
   // TODO maybe don't send a message port only to get object url?
-  // TODO dispose rpc to avoid memory leak
-  const rpc = await rpcPromise
-  return rpc
+  // TODO dispose ipc to avoid memory leak
+  HandleIpc.handleIpc(ipc)
+  return ipc
 }
