0a0fcd0460b6b7edb2a8597a5e27db5f39405cae
Le Vivilet <levivlet@pm.me>
2024-12-29 08:41:31 +0100

    feature: use npm package for webview ipc

--- packages/extension-host-worker/src/parts/ExtensionHostWebView/ExtensionHostWebView.ts
+++ packages/extension-host-worker/src/parts/ExtensionHostWebView/ExtensionHostWebView.ts
@@ -1,6 +1,5 @@
-import * as Callback from '../Callback/Callback.ts'
+import { MessagePortRpcParent } from '@lvce-editor/rpc'
 import * as ExtensionHostWebViewState from '../ExtensionHostWebViewState/ExtensionHostWebViewState.ts'
-import * as WaitForFirstMessage from '../WaitForFirstMessage/WaitForFirstMessage.ts'
 
 // TODO pass uuid to allow having multiple webviews open at the same time
 export const createWebView = async (providerId: string, port: MessagePort, uri: string, uid: number, origin: string, webView: any): Promise<void> => {
@@ -11,58 +10,25 @@ export const createWebView = async (providerId: string, port: MessagePort, uri: 
 
   // TODO cancel promise when webview is disposed before sending message
   // TODO handle case when webview doesn't send ready message
-  // TODO handle error
-  const firstMessage = await WaitForFirstMessage.waitForFirstMessage(port)
-  // @ts-ignore
-  if (firstMessage.data !== 'ready') {
-    // TODO handle error
-  }
 
-  // TODO use ipc module
-  const handlePortMessage = async (event) => {
-    const { data, target } = event
-    const { method, params, id } = data
-    if (id && !method) {
-      Callback.resolve(id, data)
-      return
-    }
-    if (provider && provider.commands && provider.commands[method]) {
-      const fn = provider.commands[method]
-      const result = await fn(...params)
-      if (id && method) {
-        target.postMessage({
-          jsonrpc: '2.0',
-          id,
-          result,
-        })
-      }
-    }
-  }
+  const rpc = await MessagePortRpcParent.create({
+    messagePort: port,
+    isMessagePortOpen: false,
+    commandMap: provider.commands || {},
+  })
 
-  port.onmessage = handlePortMessage
-  const rpc = {
+  const outer = {
     uri,
     provider,
     uid,
     origin,
     webView,
     async invoke(method, ...params) {
-      const { id, promise } = Callback.registerPromise()
-      port.postMessage({
-        jsonrpc: '2.0',
-        id,
-        method,
-        params,
-      })
-      const result = await promise
-      if (result.error) {
-        throw new Error(`Error: ${result.error.message}`)
-      }
-      return result.result
+      return rpc.invoke(method, ...params)
     },
   }
   // TODO allow creating multiple webviews per provider
-  ExtensionHostWebViewState.setWebView(providerId, rpc)
+  ExtensionHostWebViewState.setWebView(providerId, outer)
 }
 
 export const disposeWebView = (id) => {
