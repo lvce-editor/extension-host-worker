099f1a48869a9fb3f60520a65626a90a612faba3
Le Vivilet <levivlet@pm.me>
2024-12-29 11:46:59 +0100

    feature: remove unused code

--- packages/extension-host-worker/src/parts/WaitForFirstMessage/WaitForFirstMessage.ts
+++
@@ -1,6 +0,0 @@
-export const waitForFirstMessage = async (port: MessagePort): Promise<MessageEvent> => {
-  const { resolve, promise } = Promise.withResolvers()
-  port.onmessage = resolve
-  const firstMessage = await promise
-  return firstMessage as MessageEvent
-}
