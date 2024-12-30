f916e5413342a003c5d62f31a1312a3f56ea73b9
Le Vivilet <levivlet@pm.me>
2024-12-30 15:31:08 +0100

    fix: commands registration

--- packages/extension-host-sub-worker/src/parts/CommandState/CommandState.js
+++ packages/extension-host-sub-worker/src/parts/CommandState/CommandState.js
@@ -3,3 +3,5 @@ import * as CommandMap from '@lvce-editor/command'
 export const registerCommands = (commandMap) => {
   CommandMap.register(commandMap)
 }
+
+export const execute = CommandMap.execute
