cf8922fd40c6d540bc8d302aedca832360cb3947
Le Vivilet <levivlet@pm.me>
2024-12-30 16:58:36 +0100

    feature: add compatRendererWorkerInvokeAndTransfer function

--- packages/extension-host-worker/src/parts/LaunchIframeWorker/LaunchIframeWorker.ts
+++ packages/extension-host-worker/src/parts/LaunchIframeWorker/LaunchIframeWorker.ts
@@ -2,6 +2,7 @@ import * as GetConfiguredIframeWorkerUrl from '../GetConfiguredIframeWorkerUrl/G
 import * as Id from '../Id/Id.ts'
 import * as IpcParent from '../IpcParent/IpcParent.ts'
 import * as IpcParentType from '../IpcParentType/IpcParentType.ts'
+import * as GetRemoteUrlForWebView from '../GetRemoteUrlForWebView/GetRemoteUrlForWebView.ts'
 import * as Rpc from '../Rpc/Rpc.ts'
 
 const commandMap = {
@@ -10,12 +11,15 @@ const commandMap = {
     Rpc.invokeAndTransfer('WebView.compatExtensionHostWorkerInvokeAndTransfer', ...args),
   'WebView.compatRendererProcessInvoke': (...args) => Rpc.invoke('WebView.compatRendererProcessInvoke', ...args),
   'WebView.compatRendererProcessInvokeAndTransfer': (...args) => Rpc.invokeAndTransfer('WebView.compatRendererProcessInvokeAndTransfer', ...args),
+  // @ts-ignore
+  'WebView.compatRendererWorkerInvokeAndTransfer': (...args: any[]) => Rpc.invokeAndTransfer(...args),
   'WebView.compatSharedProcessInvoke': (...args) => Rpc.invoke('WebView.compatSharedProcessInvoke', ...args),
   'WebView.getSavedState': (...args) => Rpc.invoke('WebView.getSavedState', ...args),
   'WebView.getWebViewInfo': (...args) => Rpc.invoke('WebView.getWebViewInfo', ...args),
   'WebView.getWebViews': (...args) => Rpc.invoke('WebView.getWebViews', ...args),
   'WebView.setPort': (...args) => Rpc.invoke('WebView.setPort', ...args),
   'ExtensionHostManagement.activateByEvent': (...args) => Rpc.invoke('ExtensionHostManagement.activateByEvent', ...args),
+  'WebView.getRemoteUrl': (options) => GetRemoteUrlForWebView.getRemoteUrlForWebView(options.uri, options),
 }
 
 export const launchIframeWorker = async () => {
