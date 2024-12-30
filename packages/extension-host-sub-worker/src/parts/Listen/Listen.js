bbe8cff0539ed9a8431b7a3ac37357cee01c28e7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-30 15:10:22 +0100

    feature: use npm package for rpc (#150)

--- packages/extension-host-sub-worker/src/parts/Listen/Listen.js
+++ packages/extension-host-sub-worker/src/parts/Listen/Listen.js
@@ -1,19 +1,9 @@
-import * as Command from '../Command/Command.js'
+import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.js'
-import * as CommandState from '../CommandState/CommandState.js'
-import * as HandleIpc from '../HandleIpc/HandleIpc.js'
-import * as IpcChild from '../IpcChild/IpcChild.js'
-import * as IpcChildType from '../IpcChildType/IpcChildType.js'
-import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 
 export const listen = async () => {
-  CommandState.registerCommands(CommandMap.commandMap)
-  const ipc = await IpcChild.listen({ method: IpcChildType.Auto() })
-  HandleIpc.handleIpc(ipc, Command.execute)
-  const rpc = {
-    invoke(method, ...params) {
-      return JsonRpc.invoke(ipc, method, ...params)
-    },
-  }
+  const rpc = await WebWorkerRpcClient.create({
+    commandMap: CommandMap.commandMap,
+  })
   globalThis.rpc = rpc
 }
