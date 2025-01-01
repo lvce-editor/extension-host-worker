4d3ae92253671fca52a6fb24074b2d439eea9047
Le Vivilet <levivlet@pm.me>
2025-01-01 16:27:20 +0100

    move code

--- packages/extension-host-worker/src/parts/TextSearchInText/TextSearchInText.ts
+++ packages/extension-host-worker/src/parts/TextSearchInText/TextSearchInText.ts
@@ -1,26 +1,18 @@
-import { MatchCase } from '../SearchFlags/SearchFlags.ts'
+import * as GetLineMatch from '../GetLineMatch/GetLineMatch.ts'
+import { MatchCase, UseRegularExpression } from '../SearchFlags/SearchFlags.ts'
 import type { SearchResult } from '../SearchResult/SearchResult.ts'
 import * as SplitLines from '../SplitLines/SplitLines.ts'
 import * as TextSearchResultType from '../TextSearchResultType/TextSearchResultType.ts'
 
 export const textSearchInText = (file: string, content: string, query: string, flags: number = 0): readonly SearchResult[] => {
   const matchCase = flags & MatchCase
+  const useRegularExpression = flags & UseRegularExpression
   const results: SearchResult[] = []
   const lines = SplitLines.splitLines(content)
-  const actualQuery = matchCase ? query : query.toLowerCase()
+  const queryLower = query.toLowerCase()
   for (let i = 0; i < lines.length; i++) {
     const line = lines[i]
-    const lineToQuery = matchCase ? line : line.toLowerCase()
-    const index = lineToQuery.indexOf(actualQuery)
-    if (index !== -1) {
-      results.push({
-        type: TextSearchResultType.Match,
-        text: line,
-        start: index,
-        end: index + query.length,
-        lineNumber: i,
-      })
-    }
+    GetLineMatch.getLineMatch(results, line, i, query, queryLower, useRegularExpression, matchCase)
   }
   if (results.length > 0) {
     results.unshift({
