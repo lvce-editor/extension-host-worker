715fc5bf1eae0fbf947e92eefabce4a06dcb5db5
Le Vivilet <levivlet@pm.me>
2025-01-01 18:10:14 +0100

    feature: make getLineMatch function more functional

--- packages/extension-host-worker/src/parts/TextSearchInText/TextSearchInText.ts
+++ packages/extension-host-worker/src/parts/TextSearchInText/TextSearchInText.ts
@@ -7,21 +7,21 @@ import * as TextSearchResultType from '../TextSearchResultType/TextSearchResultT
 export const textSearchInText = (file: string, content: string, query: string, flags: number = 0): readonly SearchResult[] => {
   const matchCase = flags & MatchCase
   const useRegularExpression = flags & UseRegularExpression
-  const results: SearchResult[] = []
   const lines = SplitLines.splitLines(content)
   const queryLower = query.toLowerCase()
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i]
-    GetLineMatch.getLineMatch(results, line, i, query, queryLower, useRegularExpression, matchCase)
-  }
+  const results = lines.flatMap((line, i) => GetLineMatch.getLineMatch(line, i, query, queryLower, useRegularExpression, matchCase))
+
   if (results.length > 0) {
-    results.unshift({
-      type: TextSearchResultType.File,
-      text: file,
-      start: 0,
-      end: 0,
-      lineNumber: 0,
-    })
+    return [
+      {
+        type: TextSearchResultType.File,
+        text: file,
+        start: 0,
+        end: 0,
+        lineNumber: 0,
+      },
+      ...results,
+    ]
   }
   return results
 }
