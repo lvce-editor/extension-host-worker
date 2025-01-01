9f6da3fad5cc1a55c9adce0f9cfa2c4fe9762627
Le Vivilet <levivlet@pm.me>
2025-01-01 19:21:16 +0100

    feature: throw error if search error occurs due to invalid regex

--- packages/extension-host-worker/src/parts/GetLineMatchRegex/GetLineMatchRegex.ts
+++ packages/extension-host-worker/src/parts/GetLineMatchRegex/GetLineMatchRegex.ts
@@ -3,23 +3,18 @@ import * as GetTextSearchRegex from '../GetTextSearchRegex/GetTextSearchRegex.ts
 import * as TextSearchResultType from '../TextSearchResultType/TextSearchResultType.ts'
 
 export const getLineMatchRegex = (line: string, lineNumber: number, query: string, matchCase: number): readonly SearchResult[] => {
-  try {
-    const regex = GetTextSearchRegex.getTextSearchRegex(query, matchCase)
-    const match = line.match(regex)
-    if (match && typeof match.index === 'number') {
-      return [
-        {
-          type: TextSearchResultType.Match,
-          text: line,
-          start: match.index,
-          end: match.index + match[0].length,
-          lineNumber,
-        },
-      ]
-    }
-  } catch (error) {
-    console.log({ error })
-    // Invalid regex pattern - return empty array
+  const regex = GetTextSearchRegex.getTextSearchRegex(query, matchCase)
+  const match = line.match(regex)
+  if (match && typeof match.index === 'number') {
+    return [
+      {
+        type: TextSearchResultType.Match,
+        text: line,
+        start: match.index,
+        end: match.index + match[0].length,
+        lineNumber,
+      },
+    ]
   }
   return []
 }
