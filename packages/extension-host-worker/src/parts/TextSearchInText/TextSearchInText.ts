b55436a028f76ae2c24242a527a259ea8dd383ad
Le Vivilet <levivlet@pm.me>
2025-01-01 15:43:32 +0100

    feature: support matchCase in text search provider

--- packages/extension-host-worker/src/parts/TextSearchInText/TextSearchInText.ts
+++ packages/extension-host-worker/src/parts/TextSearchInText/TextSearchInText.ts
@@ -1,13 +1,17 @@
+import { MatchCase } from '../SearchFlags/SearchFlags.ts'
 import type { SearchResult } from '../SearchResult/SearchResult.ts'
 import * as SplitLines from '../SplitLines/SplitLines.ts'
 import * as TextSearchResultType from '../TextSearchResultType/TextSearchResultType.ts'
 
-export const textSearchInText = (file: string, content: string, query: string): readonly SearchResult[] => {
+export const textSearchInText = (file: string, content: string, query: string, flags: number = 0): readonly SearchResult[] => {
+  const matchCase = flags & MatchCase
   const results: SearchResult[] = []
   const lines = SplitLines.splitLines(content)
+  const actualQuery = matchCase ? query : query.toLowerCase()
   for (let i = 0; i < lines.length; i++) {
     const line = lines[i]
-    const index = line.indexOf(query)
+    const lineToQuery = matchCase ? line : line.toLowerCase()
+    const index = lineToQuery.indexOf(actualQuery)
     if (index !== -1) {
       results.push({
         type: TextSearchResultType.Match,
--- packages/extension-host-worker/src/parts/TextSearchMemory/TextSearchMemory.ts
+++ packages/extension-host-worker/src/parts/TextSearchMemory/TextSearchMemory.ts
@@ -23,13 +23,14 @@ export const textSearch = async (scheme: string, root: string, query: string, op
   const files = FileSystemMemory.getFiles()
   const relativeRoot = root.slice('memfs://'.length)
   const allResults: any[] = []
+  const flags = options.flags || 0
   for (const [key, value] of Object.entries(files)) {
     if (!matchesUri(key, relativeRoot, options.include, options.exclude)) {
       continue
     }
     if (value.type === DirentType.File) {
       const relativeUri = key.slice(relativeRoot.length + 1)
-      const results = TextSearchInText.textSearchInText(relativeUri, value.content, query)
+      const results = TextSearchInText.textSearchInText(relativeUri, value.content, query, flags)
       allResults.push(...results)
     }
   }
