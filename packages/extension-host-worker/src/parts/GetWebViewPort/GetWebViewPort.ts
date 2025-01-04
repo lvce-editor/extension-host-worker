657fbc27bdaef793aa7b485fcba0ba3111bb4912
Le Vivilet <levivlet@pm.me>
2025-01-04 17:43:23 +0100

    feature: update imports

--- packages/extension-host-worker/src/parts/GetWebViewPort/GetWebViewPort.ts
+++ packages/extension-host-worker/src/parts/GetWebViewPort/GetWebViewPort.ts
@@ -1,5 +1,5 @@
-import * as Platform from '../Platform/Platform.js'
-import * as PlatformType from '../PlatformType/PlatformType.js'
+import * as Platform from '../Platform/Platform.ts'
+import * as PlatformType from '../PlatformType/PlatformType.ts'
 
 export const getWebViewPort = (): string => {
   if (Platform.platform === PlatformType.Web) {
