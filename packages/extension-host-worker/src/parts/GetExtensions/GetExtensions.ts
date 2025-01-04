89973ac9205aaa86465c5367e263f080eff82301
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-04 22:25:10 +0100

    feature: add getLanguages command (#212)
    * feature: add getLanguages command
    
    * import

--- packages/extension-host-worker/src/parts/GetExtensions/GetExtensions.ts
+++ packages/extension-host-worker/src/parts/GetExtensions/GetExtensions.ts
@@ -1,6 +1,7 @@
 import * as ExtensionMeta from '../ExtensionMeta/ExtensionMeta.ts'
 import * as ExtensionsCache from '../ExtensionsCache/ExtensionsCache.ts'
 
+// TODO getExtensions is still called 6 times on startup instead of 1
 export const getExtensions = () => {
   if (!ExtensionsCache.has()) {
     ExtensionsCache.set(ExtensionMeta.getExtensions())
---
+++ packages/extension-host-worker/src/parts/Languages/Languages.ts
@@ -0,0 +1,14 @@
+import * as GetExtensions from '../GetExtensions/GetExtensions.ts'
+import * as GetLanguagesFromExtension from '../GetLanguagesFromExtension/GetLanguagesFromExtension.ts'
+import { VError } from '../VError/VError.ts'
+
+export const getLanguages = async () => {
+  try {
+    const extensions = await GetExtensions.getExtensions()
+    const languages = extensions.flatMap(GetLanguagesFromExtension.getLanguagesFromExtension)
+    console.log({ languages })
+    return languages
+  } catch (error) {
+    throw new VError(error, 'Failed to load languages')
+  }
+}
