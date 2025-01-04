97e1e28b278e6345e6a7243e4bfe8a3090b48588
Le Vivilet <levivlet@pm.me>
2025-01-04 18:16:41 +0100

    sort imports

--- packages/extension-host-worker/src/parts/GetWebExtensions/GetWebExtensions.ts
+++ packages/extension-host-worker/src/parts/GetWebExtensions/GetWebExtensions.ts
@@ -1,9 +1,9 @@
 import * as ExtensionMetaState from '../ExtensionMetaState/ExtensionMetaState.ts'
+import * as GetJson from '../GetJson/GetJson.ts'
 import * as Platform from '../Platform/Platform.ts'
 import * as PlatformType from '../PlatformType/PlatformType.ts'
 import * as Rpc from '../Rpc/Rpc.js'
 import * as WebExtensionsUrl from '../WebExtensionsUrl/WebExtensionsUrl.ts'
-import * as GetJson from '../GetJson/GetJson.ts'
 
 const getSharedProcessExtensions = () => {
   return Rpc.invoke(/* ExtensionManagement.getExtensions */ 'ExtensionManagement.getExtensions')
