4bf15fbe9935b3816c1dc4d9a5de2e01e2a396fb
Le Vivilet <levivlet@pm.me>
2025-01-04 18:09:54 +0100

    add fn

---
+++ packages/extension-host-worker/src/parts/ExtensionMeta/ExtensionMeta.ts
@@ -0,0 +1,22 @@
+import * as GetWebExtensions from '../GetWebExtensions/GetWebExtensions.ts'
+import * as Platform from '../Platform/Platform.ts'
+import * as PlatformType from '../PlatformType/PlatformType.ts'
+import * as Rpc from '../Rpc/Rpc.js'
+
+const getSharedProcessExtensions = () => {
+  return Rpc.invoke(/* ExtensionManagement.getExtensions */ 'ExtensionManagement.getExtensions')
+}
+
+export const getExtensions = async () => {
+  if (Platform.platform === PlatformType.Web) {
+    const webExtensions = await GetWebExtensions.getWebExtensions()
+    return webExtensions
+  }
+  if (Platform.platform === PlatformType.Remote) {
+    const webExtensions = await GetWebExtensions.getWebExtensions()
+    const sharedProcessExtensions = await getSharedProcessExtensions()
+    return [...sharedProcessExtensions, ...webExtensions]
+  }
+  const extensions = await getSharedProcessExtensions()
+  return extensions
+}
