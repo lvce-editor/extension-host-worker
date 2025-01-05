eae53e51652498135e0688a5494bc91a4ca8db57
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-05 15:55:17 +0100

    feature: use enum for http header (#230)

--- packages/extension-host-worker/src/parts/CreateResponseFromData/CreateResponseFromData.ts
+++ packages/extension-host-worker/src/parts/CreateResponseFromData/CreateResponseFromData.ts
@@ -1,9 +1,11 @@
+import * as HttpHeader from '../HttpHeader/HttpHeader.ts'
+
 export const createResponseFromData = (data: any): Response => {
   const responseString = JSON.stringify(data, null, 2)
   const response = new Response(responseString, {
     headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': `${responseString.length}`,
+      [HttpHeader.ContentType]: 'application/json',
+      [HttpHeader.ContentLength]: `${responseString.length}`,
     },
   })
   return response
