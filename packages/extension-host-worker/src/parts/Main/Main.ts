c478792633f181f97dc30ab13f8305a28e74c09c
Le Vivilet <levivlet@pm.me>
2025-01-05 15:42:09 +0100

    fix: remove console log

--- packages/extension-host-worker/src/parts/Main/Main.ts
+++ packages/extension-host-worker/src/parts/Main/Main.ts
@@ -2,20 +2,9 @@ import * as GlobalEventListeners from '../GlobalEventListeners/GlobalEventListen
 import * as Listen from '../Listen/Listen.ts'
 
 export const main = async () => {
-  const start = performance.timeOrigin
-  const a = performance.now()
   GlobalEventListeners.setup({
     errorConstructor: Error,
     global: globalThis,
   })
   await Listen.listen()
-  const e = performance.now()
-
-  console.log({
-    times: {
-      start,
-      a,
-      e,
-    },
-  })
 }
