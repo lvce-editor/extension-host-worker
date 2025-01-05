54c8ceb7e7cfdcc2dde5bf457b5cc1e1706f66e2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-05 18:04:57 +0100

    feature: add webextension to memory cache (#233)

--- packages/extension-host-worker/src/parts/AddWebExtension/AddWebExtension.ts
+++ packages/extension-host-worker/src/parts/AddWebExtension/AddWebExtension.ts
@@ -1,3 +1,4 @@
+import * as ExtensionMetaState from '../ExtensionMetaState/ExtensionMetaState.ts'
 import * as ExtensionsCache from '../ExtensionsCache/ExtensionsCache.ts'
 import * as GetWebExtensionManifest from '../GetWebExtensionManifest/GetWebExtensionManifest.ts'
 import * as GetWebManifestPath from '../GetWebManifestPath/GetWebManifestPath.ts'
@@ -5,6 +6,8 @@ import * as GetWebManifestPath from '../GetWebManifestPath/GetWebManifestPath.ts
 export const addWebExtension = async (path: string): Promise<any> => {
   const manifestPath = GetWebManifestPath.getWebManifestPath(path)
   const manifest = await GetWebExtensionManifest.getWebExtensionManifest(path, manifestPath)
+  // TODO avoid mutation if possible
+  ExtensionMetaState.state.webExtensions.push(manifest)
   ExtensionsCache.clear()
   return manifest
 }
