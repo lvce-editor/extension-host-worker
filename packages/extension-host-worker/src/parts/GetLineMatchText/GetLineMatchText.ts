b00f6039aff3f28e532c71df5689f68fbeba9017
Le Vivilet <levivlet@pm.me>
2025-01-25 00:21:54 +0100

    feature: support match whole word

--- packages/extension-host-worker/src/parts/GetLineMatchText/GetLineMatchText.ts
+++ packages/extension-host-worker/src/parts/GetLineMatchText/GetLineMatchText.ts
@@ -1,6 +1,14 @@
 import type { SearchResult } from '../SearchResult/SearchResult.ts'
 import * as TextSearchResultType from '../TextSearchResultType/TextSearchResultType.ts'
 
+const doesMatchWord = (line: string, index: number, wordBoundaries: readonly string[]): boolean => {
+  if (index === 0) {
+    return true
+  }
+  const char = line.charAt(index - 1)
+  return wordBoundaries.includes(char)
+}
+
 export const getLineMatchText = (
   line: string,
   lineNumber: number,
@@ -9,19 +17,24 @@ export const getLineMatchText = (
   matchCase: number,
   matchWholeWord: number,
 ): readonly SearchResult[] => {
+  // TODO support multiple matches per line
   const lineToQuery = matchCase ? line : line.toLowerCase()
   const actualQuery = matchCase ? query : queryLower
   const index = lineToQuery.indexOf(actualQuery)
-  if (index !== -1) {
-    return [
-      {
-        type: TextSearchResultType.Match,
-        text: line,
-        start: index,
-        end: index + query.length,
-        lineNumber,
-      },
-    ]
+  if (index === -1) {
+    return []
   }
-  return []
+  const wordBoundaries = [' ', '\t', '-']
+  if (matchWholeWord && !doesMatchWord(line, index, wordBoundaries)) {
+    return []
+  }
+  return [
+    {
+      type: TextSearchResultType.Match,
+      text: line,
+      start: index,
+      end: index + query.length,
+      lineNumber,
+    },
+  ]
 }
