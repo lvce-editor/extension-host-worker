f43da9fe2f63a53dec0b774975dee5e58ea1c862
Le Vivilet <levivlet@pm.me>
2025-01-25 00:07:12 +0100

    feature: pass matchWholeWord option

--- packages/extension-host-worker/src/parts/TextSearchInText/TextSearchInText.ts
+++ packages/extension-host-worker/src/parts/TextSearchInText/TextSearchInText.ts
@@ -1,15 +1,16 @@
 import type { SearchResult } from '../SearchResult/SearchResult.ts'
 import * as GetLineMatch from '../GetLineMatch/GetLineMatch.ts'
-import { MatchCase, UseRegularExpression } from '../SearchFlags/SearchFlags.ts'
+import { MatchCase, MatchWholeWord, UseRegularExpression } from '../SearchFlags/SearchFlags.ts'
 import * as SplitLines from '../SplitLines/SplitLines.ts'
 import * as TextSearchResultType from '../TextSearchResultType/TextSearchResultType.ts'
 
 export const textSearchInText = (file: string, content: string, query: string, flags: number = 0): readonly SearchResult[] => {
   const matchCase = flags & MatchCase
   const useRegularExpression = flags & UseRegularExpression
+  const matchWholeWord = flags & MatchWholeWord
   const lines = SplitLines.splitLines(content)
   const queryLower = query.toLowerCase()
-  const results = lines.flatMap((line, i) => GetLineMatch.getLineMatch(line, i, query, queryLower, useRegularExpression, matchCase))
+  const results = lines.flatMap((line, i) => GetLineMatch.getLineMatch(line, i, query, queryLower, useRegularExpression, matchCase, matchWholeWord))
 
   if (results.length > 0) {
     return [
