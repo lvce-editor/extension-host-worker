8e46939b929eeeb65323c50a0ff28df52a550449
Le Vivilet <levivlet@pm.me>
2025-02-21 19:53:35 +0100

    feature: move textSearchRecursively into separate file

--- packages/extension-host-worker/src/parts/TextSearchHtml/TextSearchHtml.ts
+++ packages/extension-host-worker/src/parts/TextSearchHtml/TextSearchHtml.ts
@@ -1,30 +1,8 @@
 import type { SearchResult } from '../SearchResult/SearchResult.ts'
 import * as Assert from '../Assert/Assert.ts'
-import * as FileHandleType from '../FileHandleType/FileHandleType.ts'
 import { FileNotFoundError } from '../FileNotFoundError/FileNotFoundError.ts'
-import * as FileSystemHtml from '../FileSystemHtml/FileSystemHtml.ts'
 import * as GetDirectoryHandle from '../GetDirectoryHandle/GetDirectoryHandle.ts'
-import * as TextSearchInFile from '../TextSearchInFile/TextSearchInFile.ts'
-
-const textSearchRecursively = async (all: SearchResult[], parent: string, handle: FileSystemHandle, query: string): Promise<void> => {
-  const childHandles: readonly FileSystemHandle[] = await FileSystemHtml.getChildHandles(handle)
-  const promises: Promise<void>[] = []
-  for (const childHandle of childHandles) {
-    const absolutePath = parent + '/' + childHandle.name
-    switch (childHandle.kind) {
-      case FileHandleType.Directory:
-        promises.push(textSearchRecursively(all, absolutePath, childHandle, query))
-        break
-      case FileHandleType.File:
-        // @ts-ignore
-        promises.push(TextSearchInFile.textSearchInFile(all, childHandle, absolutePath, query))
-        break
-      default:
-        break
-    }
-  }
-  await Promise.all(promises)
-}
+import * as TextSearchRecursively from '../TextSearchRecursively/TextSearchRecursively.ts'
 
 export const textSearch = async (scheme: string, root: string, query: string): Promise<readonly SearchResult[]> => {
   Assert.string(scheme)
@@ -36,6 +14,6 @@ export const textSearch = async (scheme: string, root: string, query: string): P
     throw new FileNotFoundError(relativeRoot)
   }
   const all: SearchResult[] = []
-  await textSearchRecursively(all, '', handle, query)
+  await TextSearchRecursively.textSearchRecursively(all, '', handle, query)
   return all
 }
