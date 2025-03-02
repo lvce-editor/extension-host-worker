51fcabbce07156891bf9a99aa624a6fed61597ff
Le Vivilet <levivlet@pm.me>
2025-03-02 13:33:12 +0100

    feature: move walk into separate file

--- packages/extension-host-worker/src/parts/GetBabelAstDependencies/GetBabelAstDependencies.ts
+++ packages/extension-host-worker/src/parts/GetBabelAstDependencies/GetBabelAstDependencies.ts
@@ -1,51 +1,5 @@
 import * as BabelNodeType from '../BabelNodeType/BabelNodeType.ts'
-
-const walk = (node, visitor) => {
-  if (!node) {
-    return
-  }
-  if (Array.isArray(node)) {
-    for (const item of node) {
-      walk(item, visitor)
-    }
-    return
-  }
-  visitor(node)
-  switch (node.type) {
-    case BabelNodeType.File:
-      walk(node.program, visitor)
-      break
-    case BabelNodeType.Program:
-      walk(node.body, visitor)
-      break
-    case BabelNodeType.ExportNamedDeclaration:
-      walk(node.declaration, visitor)
-      break
-    case BabelNodeType.VariableDeclaration:
-      walk(node.declarations, visitor)
-      break
-    case BabelNodeType.VariableDeclarator:
-      walk(node.init, visitor)
-      break
-    case BabelNodeType.ArrowFunctionExpression:
-      walk(node.body, visitor)
-      break
-    case BabelNodeType.BlockStatement:
-      walk(node.body, visitor)
-      break
-    case BabelNodeType.ExpressionStatement:
-      walk(node.expression, visitor)
-      break
-    case BabelNodeType.AwaitExpression:
-      walk(node.argument, visitor)
-      break
-    case BabelNodeType.CallExpression:
-      walk(node.callee, visitor)
-      break
-    default:
-      break
-  }
-}
+import * as Walk from '../Walk/Walk.ts'
 
 export const getBabelAstDependencies = (code, ast) => {
   const { program } = ast
@@ -98,6 +52,6 @@ export const getBabelAstDependencies = (code, ast) => {
       dependencies.push({ relativePath, code, start, end })
     }
   }
-  walk(ast, visitor)
+  Walk.walk(ast, visitor)
   return dependencies
 }
