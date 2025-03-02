ff6f393f70ac801f26ada205d7b5d9dffa7239d1
Le Vivilet <levivlet@pm.me>
2025-03-02 13:45:43 +0100

    feature: add csp tests

---
+++ packages/extension-host-worker/test/ContentSecurityPolicyErrorState.test.ts
@@ -0,0 +1,28 @@
+import { expect, test } from '@jest/globals'
+import * as ContentSecurityPolicyErrorState from '../src/parts/ContentSecurityPolicyErrorState/ContentSecurityPolicyErrorState.ts'
+
+test('addError', () => {
+  const error = {
+    violatedDirective: 'script-src-elem',
+    sourceFile: 'http://localhost:3000/test.ts',
+    lineNumber: 1,
+    columnNumber: 11,
+  }
+  ContentSecurityPolicyErrorState.addError(error)
+  expect(ContentSecurityPolicyErrorState.hasRecentErrors()).toBe(true)
+})
+
+test('hasRecentErrors - no errors', () => {
+  expect(ContentSecurityPolicyErrorState.hasRecentErrors()).toBe(false)
+})
+
+test('getRecentError', () => {
+  const error = {
+    violatedDirective: 'script-src-elem',
+    sourceFile: 'http://localhost:3000/test.ts',
+    lineNumber: 1,
+    columnNumber: 11,
+  }
+  ContentSecurityPolicyErrorState.addError(error)
+  expect(ContentSecurityPolicyErrorState.getRecentError()).toEqual(error)
+})
