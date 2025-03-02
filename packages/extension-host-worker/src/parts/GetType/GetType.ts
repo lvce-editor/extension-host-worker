16cc2941727abb1740fd109d4f178d38a9309f21
Le Vivilet <levivlet@pm.me>
2025-03-02 13:16:55 +0100

    feature: move getType into separate file

---
+++ packages/extension-host-worker/src/parts/GetType/GetType.ts
@@ -0,0 +1,24 @@
+export const getType = (value): string => {
+  switch (typeof value) {
+    case 'number':
+      return 'number'
+    case 'function':
+      return 'function'
+    case 'string':
+      return 'string'
+    case 'object':
+      if (value === null) {
+        return 'null'
+      }
+      if (Array.isArray(value)) {
+        return 'array'
+      }
+      return 'object'
+    case 'boolean':
+      return 'boolean'
+    case 'undefined':
+      return 'undefined'
+    default:
+      return 'unknown'
+  }
+}
