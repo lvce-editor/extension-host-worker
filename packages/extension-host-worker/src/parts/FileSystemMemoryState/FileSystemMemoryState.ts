2f79eb93befaebb8c457980cc4626c6896c9577b
Le Vivilet <levivlet@pm.me>
2025-03-02 13:51:40 +0100

    feature: move in memory file system state into separate file \

---
+++ packages/extension-host-worker/src/parts/FileSystemMemoryState/FileSystemMemoryState.ts
@@ -0,0 +1,21 @@
+import type { InMemoryFile } from '../InMemoryFile/InMemoryFile.ts'
+
+// TODO move this to an extension?
+
+const files: Record<string, InMemoryFile> = Object.create(null)
+
+export const getDirent = (uri: string): InMemoryFile => {
+  return files[uri]
+}
+
+export const setDirent = (uri: string, dirent: InMemoryFile): void => {
+  files[uri] = dirent
+}
+
+export const getAll = () => {
+  return files
+}
+
+export const remove = (uri: string): void => {
+  delete files[uri]
+}
