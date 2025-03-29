bf9518f2d57daf5f944162b18ac8a163e49e2476
Le Vivilet <levivlet@pm.me>
2025-03-29 08:58:56 +0100

    feature: use more exact types

--- packages/extension-host-worker/src/parts/SearchFileWithHtml/SearchFileWithHtml.ts
+++ packages/extension-host-worker/src/parts/SearchFileWithHtml/SearchFileWithHtml.ts
@@ -6,7 +6,6 @@ export const searchFile = async (uri: string): Promise<readonly string[]> => {
   const path = uri.slice('html://'.length)
   const handle = await GetDirectoryHandle.getDirectoryHandle(path)
   if (!handle) {
-    // @ts-ignore
     throw new VError(`Folder not found ${uri}`)
   }
   const all: any[] = []
