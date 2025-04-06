1d8e64dc8ace5628859d1806315bae213e884c3e
Le Vivilet <levivlet@pm.me>
2025-04-06 17:52:19 +0200

    feature: improve memory file search

--- packages/extension-host-worker/src/parts/SearchFileWithMemory/SearchFileWithMemory.ts
+++ packages/extension-host-worker/src/parts/SearchFileWithMemory/SearchFileWithMemory.ts
@@ -1,11 +1,7 @@
-import * as Assert from '../Assert/Assert.ts'
 import * as DirentType from '../DirentType/DirentType.ts'
 import * as FileSystemMemory from '../FileSystemMemory/FileSystemMemory.ts'
 
 export const searchFile = async (scheme: string, root: string, query: string, options: any, assetDir: string): Promise<readonly string[]> => {
-  Assert.string(scheme)
-  Assert.string(root)
-  Assert.string(query)
   const files = FileSystemMemory.getFiles()
   const allResults: string[] = []
   for (const [key, value] of Object.entries(files)) {
