28eb3933984be56237bd32ff6982a41363e3e6f7
Le Vivilet <levivlet@pm.me>
2025-04-30 19:22:26 +0200

    feature: disallow console logs

--- packages/extension-host-worker/src/parts/BulkReplacement/BulkReplacement.ts
+++ packages/extension-host-worker/src/parts/BulkReplacement/BulkReplacement.ts
@@ -1,4 +1,5 @@
 export const applyBulkReplacement = async (files, ranges, replacement): Promise<void> => {
+  // eslint-disable-next-line no-console
   console.log({ files, ranges, replacement })
   throw new Error('not implemented')
 }
