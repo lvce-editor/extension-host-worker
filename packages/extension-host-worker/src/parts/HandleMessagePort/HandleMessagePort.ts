7aaae4e2c36ca86286a1c99d2b91c96a7236515b
Le Vivilet <levivlet@pm.me>
2025-04-30 19:29:04 +0200

    feature: store message port from debug worker as debug worker rpc

--- packages/extension-host-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
+++ packages/extension-host-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
@@ -1,8 +1,12 @@
 import { MessagePortRpcClient } from '@lvce-editor/rpc'
+import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
 
-export const handleMessagePort = async (port: MessagePort) => {
-  await MessagePortRpcClient.create({
+export const handleMessagePort = async (port: MessagePort, rpcId?: number) => {
+  const rpc = await MessagePortRpcClient.create({
     messagePort: port,
     commandMap: {},
   })
+  if (rpcId) {
+    RpcRegistry.set(rpcId, rpc)
+  }
 }
