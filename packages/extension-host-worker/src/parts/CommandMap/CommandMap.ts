5530fa0ec4ad5aef41f5c6ed33b2d78c444af6bc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-03 14:42:27 +0200

    feature: add handleMessagePort2 function (#321)
    * feature: add handleMessagePort2 function
    
    * lint

--- packages/extension-host-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-host-worker/src/parts/CommandMap/CommandMap.ts
@@ -44,6 +44,7 @@ import * as GetRpcInfo from '../GetRpcInfo/GetRpcInfo.ts'
 import * as GetWebViewInfo2 from '../GetWebViewInfo2/GetWebViewInfo2.ts'
 import * as GetWebViews from '../GetWebViews/GetWebViews.ts'
 import * as HandleBeforeUnload from '../HandleBeforeUnload/HandleBeforeUnload.ts'
+import * as HandleMessagePort2 from '../HandleMessagePort2/HandleMessagePort2.ts'
 import * as HandleMessagePort from '../HandleMessagePort/HandleMessagePort.ts'
 import * as IconTheme from '../IconTheme/IconTheme.ts'
 import * as IconThemeState from '../IconThemeState/IconThemeState.ts'
@@ -107,7 +108,8 @@ export const commandMap = {
   'FileSystemMemory.remove': FileSystemMemory.remove,
   'FileSystemMemory.writeFile': FileSystemMemory.writeFile,
   'HandleBeforeUnload.handleBeforeUnload': HandleBeforeUnload.handleBeforeUnload,
-  'HandleMessagePort.handleMessagePort': HandleMessagePort.handleMessagePort,
+  'HandleMessagePort2.handleMessagePort': HandleMessagePort.handleMessagePort,
+  'HandleMessagePort2.handleMessagePort2': HandleMessagePort2.handleMessagePort2,
   'IconTheme.getJson': GetIconThemeJson.getIconThemeJson,
   'IconTheme.getState': IconThemeState.getState,
   'IconTheme.hydrate': IconTheme.hydrate,
