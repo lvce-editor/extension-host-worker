4a5f165c90ada8694d0bd9f6d59a9793ad605df7
Le Vivilet <levivlet@pm.me>
2025-06-09 22:10:01 +0200

    feature: use invoke for debug messages

--- packages/extension-host-worker/src/parts/DebugRpc/DebugRpc.ts
+++
@@ -1,8 +0,0 @@
-import * as RpcRegistry from '@lvce-editor/rpc-registry'
-import * as RpcId from '../RpcId/RpcId.ts'
-
-// TODO use invoke
-export const send = async (method: string, ...params: readonly any[]): Promise<void> => {
-  const rpc = RpcRegistry.get(RpcId.DebugWorker)
-  rpc.send(method, ...params)
-}
