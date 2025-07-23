95bce94e6e4d19c4e35241f27df202d35dc0bcd5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-23 22:44:51 +0200

    feature: store runtime status in activateExtension (#389)
    * feature: store runtime status in activateExtension
    
    * fix
    
    * query status
    
    * lint
    
    * lint

--- packages/extension-host-worker/src/parts/GetRuntimeStatus/GetRuntimeStatus.ts
+++ packages/extension-host-worker/src/parts/GetRuntimeStatus/GetRuntimeStatus.ts
@@ -1,10 +1,14 @@
 import type { RuntimeStatus } from '../RuntimeStatus/RuntimeStatus.ts'
+import * as RuntimeStatusState from '../RuntimeStatusState/RuntimeStatusState.ts'
+import * as RuntimeStatusType from '../RuntimeStatusType/RuntimeStatusType.ts'
+
+const emptyStatus: RuntimeStatus = {
+  status: RuntimeStatusType.None,
+  id: '',
+  activationEvent: '',
+  activationTime: 0,
+}
 
 export const getRuntimeStatus = (extensionId: string): RuntimeStatus => {
-  return {
-    status: 'not-running',
-    id: extensionId,
-    activationEvent: '',
-    activationTime: 0,
-  }
+  return RuntimeStatusState.get(extensionId) || emptyStatus
 }
