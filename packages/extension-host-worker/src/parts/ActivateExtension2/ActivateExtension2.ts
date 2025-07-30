c894e0e76fd9632c6b1de07231d21c8ad0265575
Le Vivilet <levivlet@pm.me>
2025-07-30 10:56:14 +0200

    feature: remove done todo

--- packages/extension-host-worker/src/parts/ActivateExtension2/ActivateExtension2.ts
+++ packages/extension-host-worker/src/parts/ActivateExtension2/ActivateExtension2.ts
@@ -18,8 +18,6 @@ const rejectAfterTimeout = async (timeout, token) => {
   throw new Error(`Activation timeout of ${timeout}ms exceeded`)
 }
 
-// TODO separate importing extension and activating extension for smaller functions
-// and better error handling
 export const activateExtension2 = async (extensionId: string, extension: any) => {
   const token = CancelToken.create()
   try {
