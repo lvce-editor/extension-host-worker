13558630523fea395db540d19fc38fa8fd9966b9
Le Vivilet <levivlet@pm.me>
2025-07-30 16:41:10 +0200

    send port

--- packages/extension-host-worker/src/parts/ErrorWorker/ErrorWorker.ts
+++ packages/extension-host-worker/src/parts/ErrorWorker/ErrorWorker.ts
@@ -2,8 +2,11 @@ import { TransferMessagePortRpcParent } from '@lvce-editor/rpc'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 const send = async (port: MessagePort): Promise<void> => {
+  const command = 'Errors.handleMessagePort'
+
   // @ts-ignore
-  await RendererWorker.invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToErrorWorker', port)
+
+  await RendererWorker.invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToErrorWorker', port, command)
 }
 
 export const getImportError = async (url: string, error: any): Promise<any> => {
