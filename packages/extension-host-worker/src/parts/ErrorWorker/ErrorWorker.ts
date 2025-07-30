6e57bad80012476cac15cbd4b325699b97fc7736
Le Vivilet <levivlet@pm.me>
2025-07-30 16:35:16 +0200

    feature: try to use error worker for querying data for import error

---
+++ packages/extension-host-worker/src/parts/ErrorWorker/ErrorWorker.ts
@@ -0,0 +1,16 @@
+import { TransferMessagePortRpcParent } from '@lvce-editor/rpc'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
+
+const send = async (port: MessagePort): Promise<void> => {
+  // @ts-ignore
+  await RendererWorker.invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToErrorWorker', port)
+}
+
+export const getImportError = async (url: string, error: any): Promise<any> => {
+  const rpc = await TransferMessagePortRpcParent.create({
+    commandMap: {},
+    send,
+  })
+  const result = await rpc.invoke('Errors.tryToGetActualImportErrorMessage', url, error)
+  return result
+}
