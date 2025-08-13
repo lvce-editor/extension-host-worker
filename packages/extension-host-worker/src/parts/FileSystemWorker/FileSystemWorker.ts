7fe372e9632693149eeeccec92c7779b4129b633
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-13 22:23:01 +0200

    feature: add fs append (#413)

--- packages/extension-host-worker/src/parts/FileSystemWorker/FileSystemWorker.ts
+++ packages/extension-host-worker/src/parts/FileSystemWorker/FileSystemWorker.ts
@@ -1,3 +1,16 @@
+import type { Rpc } from '@lvce-editor/rpc'
+import { launchFileSystemProcess } from '../LaunchFileSystemProcess/LaunchFileSystemProcess.ts'
+
+let fileSystemWorkerPromise: Promise<Rpc> | undefined
+
+const getOrCreateRpc = async (): Promise<Rpc> => {
+  if (!fileSystemWorkerPromise) {
+    fileSystemWorkerPromise = launchFileSystemProcess()
+  }
+  return fileSystemWorkerPromise
+}
+
 export const append = async (uri: string, content: string): Promise<void> => {
-  // TODO ask file system workr to append
+  const rpc = await getOrCreateRpc()
+  await rpc.invoke('FileSystem.appendFile', uri, content)
 }
