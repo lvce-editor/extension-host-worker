06dfd6275edf1687ae953a1576d4dbcdeb7bdeab
Le Vivilet <levivilet@pm.me>
2025-09-10 21:39:12 +0200

    feature: better handle empty color theme (#443)

--- packages/extension-host-worker/src/parts/GetColorThemeJsonRemote/GetColorThemeJsonRemote.ts
+++ packages/extension-host-worker/src/parts/GetColorThemeJsonRemote/GetColorThemeJsonRemote.ts
@@ -5,6 +5,9 @@ import * as GetExtensions from '../GetExtensions/GetExtensions.ts'
 export const getColorThemeJson = async (colorThemeId: string): Promise<any> => {
   const extensions = await GetExtensions.getExtensions()
   const colorThemeUri = GetColorThemePath.getColorThemeUri(extensions, colorThemeId)
+  if (!colorThemeUri) {
+    return {}
+  }
   const json = await FileSystem.readJson(colorThemeUri)
   return json
 }
