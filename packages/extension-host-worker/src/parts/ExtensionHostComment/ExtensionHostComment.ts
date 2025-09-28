c272381963c534b9bfad0a8cbd83274b406e2123
Le Vivilet <levivilet@pm.me>
2025-09-28 19:16:42 +0200

    feature: update block comment api

--- packages/extension-host-worker/src/parts/ExtensionHostComment/ExtensionHostComment.ts
+++ packages/extension-host-worker/src/parts/ExtensionHostComment/ExtensionHostComment.ts
@@ -1,14 +1,10 @@
 import * as Registry from '../Registry/Registry.ts'
-import * as Types from '../Types/Types.ts'
 
 const { registerCommentProvider, executeCommentProvider } = Registry.create({
   name: 'Comment',
-  resultShape: {
-    type: Types.Array,
-    items: {
-      type: Types.String,
-    },
+  resultShape() {
+    return ''
   },
 })
 
-export { registerCommentProvider, executeCommentProvider }
+export { executeCommentProvider, registerCommentProvider }
