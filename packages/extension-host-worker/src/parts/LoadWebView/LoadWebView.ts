c2244db395895d000e122f363ab60d8fc0761900
Le Vivilet <levivilet@pm.me>
2025-12-09 14:50:51 +0100

    lint

--- packages/extension-host-worker/src/parts/LoadWebView/LoadWebView.ts
+++ packages/extension-host-worker/src/parts/LoadWebView/LoadWebView.ts
@@ -4,6 +4,7 @@ export const loadWebView = async (providerId, savedState) => {
   const rpc = ExtensionHostWebViewState.getWebView(providerId)
 
   if (rpc && rpc.webView && rpc.webView.rpc && typeof rpc.webView.rpc === 'string') {
+    // TODO
   }
   await rpc.provider.create(rpc, rpc.uri, savedState)
 }
