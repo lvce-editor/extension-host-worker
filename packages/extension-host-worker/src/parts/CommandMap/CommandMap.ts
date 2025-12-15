f3d298b00a803e15aa1c02431238e82cc478bbd2
Le Vivilet <levivilet@pm.me>
2025-12-15 15:38:58 +0100

    import

--- packages/extension-host-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-host-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,4 +1,5 @@
 import * as ActivateExtension2 from '../ActivateExtension2/ActivateExtension2.ts'
+import { activateExtension3 } from '../ActivateExtension3/ActivateExtension3.ts'
 import * as ExtensionHostExtension from '../ActivateExtension/ActivateExtension.ts'
 import * as AddWebExtension from '../AddWebExtension/AddWebExtension.ts'
 import * as BulkReplacement from '../BulkReplacement/BulkReplacement.ts'
@@ -77,7 +78,7 @@ export const commandMap = {
   'ColorTheme.hydrate': ColorTheme.hydrate,
   'ExecuteExternalCommand.executeExternalCommand': ExecuteExternalCommand.executeExternalCommand,
   'ExtensionHost.activateExtension2': ActivateExtension2.activateExtension2,
-  'ExtensionHost.activateExtension3': ActivateExtension2.activateExtension3,
+  'ExtensionHost.activateExtension3': activateExtension3,
   'ExtensionHost.getRuntimeStatus': GetRuntimeStatus.getRuntimeStatus,
   'ExtensionHost.importExtension': ImportExtension.importExtension,
   'ExtensionHost.launchIframeWorker': LaunchIframeWorker.launchIframeWorker,
