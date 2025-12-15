70e42491d213921f229c3bc243f13f79a75804f7
Le Vivilet <levivilet@pm.me>
2025-12-15 16:46:59 +0100

    feature: better handle error (#497)

--- packages/extension-host-worker/src/parts/ActivateExtension3/ActivateExtension3.ts
+++ packages/extension-host-worker/src/parts/ActivateExtension3/ActivateExtension3.ts
@@ -2,5 +2,8 @@ import * as ExtensionModules from '../ExtensionModules/ExtensionModules.ts'
 
 export const activateExtension3 = async (extensionId: string, extension: any): Promise<void> => {
   const module = ExtensionModules.acquire(extensionId)
+  if (!module) {
+    throw new Error(`extension module ${extensionId} not found`)
+  }
   await module.activate(extension)
 }
