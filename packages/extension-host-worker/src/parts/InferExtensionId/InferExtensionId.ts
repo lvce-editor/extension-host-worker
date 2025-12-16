6d4c6e9e92b93ed339614375b58e3c1d9b61c037
Le Vivilet <levivilet@pm.me>
2025-12-16 14:54:30 +0100

    feature: remove unused files

--- packages/extension-host-worker/src/parts/InferExtensionId/InferExtensionId.ts
+++
@@ -1,6 +0,0 @@
-import * as Character from '../Character/Character.ts'
-
-export const interExtensionId = (path: string): string => {
-  const slashIndex = path.lastIndexOf(Character.Slash)
-  return path.slice(slashIndex + 1)
-}
