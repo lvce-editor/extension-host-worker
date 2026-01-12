3ce7c23082f69adcdc57dcec9cbea8cedf57a28b
Le Vivilet <levivilet@pm.me>
2026-01-12 19:59:14 +0100

    feature: launch workers in parallel

--- packages/extension-host-worker/src/parts/LaunchFileSearchWorker/LaunchFileSearchWorker.ts
+++ packages/extension-host-worker/src/parts/LaunchFileSearchWorker/LaunchFileSearchWorker.ts
@@ -1,14 +1,16 @@
-import type { Rpc } from '@lvce-editor/rpc'
 import { LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
-import { RendererWorker } from '@lvce-editor/rpc-registry'
+import { FileSearchWorker, RendererWorker } from '@lvce-editor/rpc-registry'
 
-export const launchFileSearchWorker = async (): Promise<Rpc> => {
-  const rpc = await LazyTransferMessagePortRpcParent.create({
-    commandMap: {},
-    async send(port) {
-      // @ts-ignore
-      await RendererWorker.sendMessagePortToFileSearchWorker(port, 0)
-    },
-  })
-  return rpc
+export const launchFileSearchWorker = async (): Promise<void> => {
+  try {
+    const rpc = await LazyTransferMessagePortRpcParent.create({
+      commandMap: {},
+      async send(port) {
+        await RendererWorker.sendMessagePortToFileSearchWorker(port, 0)
+      },
+    })
+    FileSearchWorker.set(rpc)
+  } catch {
+    // ignore
+  }
 }
--- packages/extension-host-worker/src/parts/Listen/Listen.ts
+++ packages/extension-host-worker/src/parts/Listen/Listen.ts
@@ -1,14 +1,7 @@
-import { WebWorkerRpcClient } from '@lvce-editor/rpc'
-import * as CommandMap from '../CommandMap/CommandMap.ts'
 import { launchExtensionManagementWorker } from '../LaunchExtensionManagementWorker/LaunchExtensionManagementWorker.ts'
-import * as RpcId from '../RpcId/RpcId.ts'
-import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
+import { launchFileSearchWorker } from '../LaunchFileSearchWorker/LaunchFileSearchWorker.ts'
+import { launchRendererWorker } from '../LaunchRendererWorker/LaunchRendererWorker.ts'
 
 export const listen = async (): Promise<void> => {
-  const rpc1 = await launchExtensionManagementWorker()
-  RpcRegistry.ExtensionManagementWorker.set(rpc1)
-  const rpc = await WebWorkerRpcClient.create({
-    commandMap: CommandMap.commandMap,
-  })
-  RpcRegistry.set(RpcId.RendererWorker, rpc)
+  await Promise.all([launchExtensionManagementWorker(), launchFileSearchWorker(), launchRendererWorker()])
 }
