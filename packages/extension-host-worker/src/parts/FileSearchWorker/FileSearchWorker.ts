4fc72c8d0ae231c0099b2210c568ca8ec591ba48
Le Vivilet <levivilet@pm.me>
2026-01-12 17:48:47 +0100

    feature: update error message

--- packages/extension-host-worker/src/parts/FileSearchWorker/FileSearchWorker.ts
+++ packages/extension-host-worker/src/parts/FileSearchWorker/FileSearchWorker.ts
@@ -3,7 +3,7 @@ import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
 
 export const invoke = (method: string, ...params: any[]): Promise<any> => {
   // TODO
-  throw new Error(`not implemented`)
+  throw new Error(`[file search worker invoke] not implemented`)
 }
 
 export const invokeAndTransfer = (method: string, ...params: any[]): Promise<any> => {
