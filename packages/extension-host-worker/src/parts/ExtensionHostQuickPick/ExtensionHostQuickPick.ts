a33bb27523162976a052ac62e78865a5a5859955
Le Vivilet <levivilet@pm.me>
2026-01-12 16:31:57 +0100

    feature: add showQuickInput api

--- packages/extension-host-worker/src/parts/ExtensionHostQuickPick/ExtensionHostQuickPick.ts
+++ packages/extension-host-worker/src/parts/ExtensionHostQuickPick/ExtensionHostQuickPick.ts
@@ -1,3 +1,5 @@
+import * as FileSearchWorker from '../FileSearchWorker/FileSearchWorker.ts'
+import * as Id from '../Id/Id.ts'
 import * as RendererWorkerCommandType from '../RendererWorkerCommandType/RendererWorkerCommandType.ts'
 import * as Rpc from '../Rpc/Rpc.ts'
 
@@ -5,4 +7,33 @@ export const showQuickPick = async ({ getPicks, toPick }) => {
   const rawPicks = await getPicks()
   const picks = rawPicks.map(toPick)
   return Rpc.invoke(RendererWorkerCommandType.ExtensionHostQuickPickShow, picks)
+}
+
+const quickInputs = Object.create(null)
+
+interface QuickInputOptions {
+  readonly ignoreFocusOut?: boolean
+  readonly initialValue?: string
+  readonly render?: any
+}
+
+interface QuickInputResult {
+  readonly canceled: boolean
+  readonly inputValue: string
+}
+
+export const showQuickInput = async ({ ignoreFocusOut, initialValue, render }: QuickInputOptions): Promise<QuickInputResult> => {
+  const id = Id.create()
+  quickInputs[id] = render
+  // TODO create direct connection to file search worker
+  const { canceled, inputValue } = await FileSearchWorker.invoke('QuickPick.showQuickInput', {
+    ignoreFocusOut,
+    initialValue,
+    render,
+  })
+  delete quickInputs[id]
+  return {
+    canceled,
+    inputValue,
+  }
 }
---
+++ packages/extension-host-worker/src/parts/FileSearchWorker/FileSearchWorker.ts
@@ -0,0 +1,12 @@
+import * as RpcId from '../RpcId/RpcId.ts'
+import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
+
+export const invoke = (method: string, ...params: any[]): Promise<any> => {
+  // TODO
+  throw new Error(`not implemented`)
+}
+
+export const invokeAndTransfer = (method: string, ...params: any[]): Promise<any> => {
+  const rpc = RpcRegistry.get(RpcId.RendererWorker)
+  return rpc.invokeAndTransfer(method, ...params)
+}
