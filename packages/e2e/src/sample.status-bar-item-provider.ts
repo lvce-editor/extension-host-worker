2e9bf8cc0f15666981182a9f6d405edbacd1c07f
Le Vivilet <levivilet@pm.me>
2026-05-27 00:06:13 +0200

    fix: remove unused FileSystem parameter and clean up test function (#557)

--- packages/e2e/src/sample.status-bar-item-provider.ts
+++ packages/e2e/src/sample.status-bar-item-provider.ts
@@ -4,15 +4,12 @@ export const name = 'sample.status-bar-item-provider'
 
 export const skip = 1
 
-export const test: Test = async ({ Extension, FileSystem, Locator, expect }) => {
+export const test: Test = async ({ Extension, Locator, expect }) => {
   // arrange
   const uri = import.meta.resolve(`../fixtures/${name}`)
   await Extension.addWebExtension(uri)
-  // @ts-ignore
-  const tmpDir = await FileSystem.getTmpDir()
 
   // assert
   const item = Locator('.StatusBarItem[name="xyz"]')
   await expect(item).toBeVisible()
-  // TODO wait for status bar item to be visible
 }
