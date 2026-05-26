28ab2ae7ac499542d3d602cf36b5b142b18f4293
Le Vivilet <levivilet@pm.me>
2026-05-26 21:03:08 +0200

    feat: add sample status bar item provider test implementation (#551)
    * feat: add sample status bar item provider test implementation
    
    * log

---
+++ packages/e2e/src/sample.status-bar-item-provider.ts
@@ -0,0 +1,18 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'sample.status-bar-item-provider'
+
+export const skip = 1
+
+export const test: Test = async ({ Extension, FileSystem, Locator, expect }) => {
+  // arrange
+  const uri = import.meta.resolve(`../fixtures/${name}`)
+  await Extension.addWebExtension(uri)
+  // @ts-ignore
+  const tmpDir = await FileSystem.getTmpDir()
+
+  // assert
+  const item = Locator('.StatusBarItem[name="xyz"]')
+  await expect(item).toBeVisible()
+  // TODO wait for status bar item to be visible
+}
