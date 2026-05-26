20f375ab0b495c74c5c8f13fcb8f9f38aa996afb
Le Vivilet <levivilet@pm.me>
2026-05-27 00:04:10 +0200

    feat: update test implementation to include QuickPick command execution

--- packages/e2e/src/sample.status-bar-item-provider-click.ts
+++ packages/e2e/src/sample.status-bar-item-provider-click.ts
@@ -4,15 +4,16 @@ export const name = 'sample.status-bar-item-provider-click'
 
 export const skip = 1
 
-export const test: Test = async ({ Extension, FileSystem, Locator, expect }) => {
+export const test: Test = async ({ Extension, Locator, expect, QuickPick }) => {
   // arrange
   const uri = import.meta.resolve(`../fixtures/${name}`)
   await Extension.addWebExtension(uri)
-  // @ts-ignore
-  const tmpDir = await FileSystem.getTmpDir()
 
   // assert
   const item = Locator('.StatusBarItem[name="xyz"]')
   await expect(item).toBeVisible()
-  // TODO wait for status bar item to be visible
+
+  await QuickPick.executeCommand(`xyz.increment`)
+
+  await expect(item).toHaveText('1')
 }
