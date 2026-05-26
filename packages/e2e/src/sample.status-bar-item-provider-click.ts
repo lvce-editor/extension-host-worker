66323c7016fa5e3d1b70306715e08f8f255a2b99
Le Vivilet <levivilet@pm.me>
2026-05-27 01:22:24 +0200

    feat: enhance test setup by adding FileSystem and Workspace dependencies (#561)
    * feat: enhance test setup by adding FileSystem and Workspace dependencies
    
    * fix: update status bar item name for consistency

--- packages/e2e/src/sample.status-bar-item-provider-click.ts
+++ packages/e2e/src/sample.status-bar-item-provider-click.ts
@@ -4,8 +4,10 @@ export const name = 'sample.status-bar-item-provider-click'
 
 export const skip = 1
 
-export const test: Test = async ({ Extension, Locator, expect, QuickPick }) => {
+export const test: Test = async ({ FileSystem, Extension, Locator, expect, QuickPick, Workspace }) => {
   // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await Workspace.setPath(tmpDir)
   const uri = import.meta.resolve(`../fixtures/${name}`)
   await Extension.addWebExtension(uri)
   const item = Locator('.StatusBarItem[name="xyz"]')
