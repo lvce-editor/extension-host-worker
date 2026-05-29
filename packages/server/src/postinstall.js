189f6cc5c1145fba92e3c7255023cb4188e4ef65
Le Vivilet <levivilet@pm.me>
2026-05-29 12:07:41 +0200

    refactor: improve postinstall script structure and variable naming

--- packages/server/src/postinstall.js
+++ packages/server/src/postinstall.js
@@ -1,8 +1,8 @@
-import { readFile, writeFile } from 'node:fs/promises'
-import { dirname, join } from 'node:path'
-import { fileURLToPath, pathToFileURL } from 'node:url'
+import { readFile, readdir, writeFile } from 'node:fs/promises'
+import { join } from 'node:path'
+import { pathToFileURL } from 'node:url'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = import.meta.dirname
 
 const root = join(__dirname, '..', '..', '..')
 
@@ -13,23 +13,27 @@ export const getRemoteUrl = (path) => {
 
 const nodeModulesPath = join(root, 'packages', 'server', 'node_modules')
 
-const extensionHostWorkerPath = join(root, '.tmp', 'dist', 'dist', 'extensionHostWorkerMain.js')
+const workerPath = join(root, '.tmp', 'dist', 'dist', 'extensionHostWorkerMain.js')
 
-const staticPath = join(nodeModulesPath, '@lvce-editor', 'static-server', 'static')
-const indexHtmlPath = join(staticPath, 'index.html')
+const serverStaticPath = join(nodeModulesPath, '@lvce-editor', 'static-server', 'static')
 
-const indexHtmlContent = await readFile(indexHtmlPath, 'utf8')
-
-const remoteUrl = getRemoteUrl(extensionHostWorkerPath)
-
-const config = {
-  'develop.extensionHostWorkerPath': remoteUrl,
+const RE_COMMIT_HASH = /^[a-z\d]+$/
+const isCommitHash = (dirent) => {
+  return dirent.length === 7 && dirent.match(RE_COMMIT_HASH)
 }
-const stringifiedConfig = JSON.stringify(config, null, 2)
-const newContent = indexHtmlContent.replace(
-  '</title>',
-  `</title>
-  <script type="application/json" id="Config">${stringifiedConfig}</script>`,
-)
 
-await writeFile(indexHtmlPath, newContent)
+const dirents = await readdir(serverStaticPath)
+const commitHash = dirents.find(isCommitHash) || ''
+const rendererWorkerMainPath = join(serverStaticPath, commitHash, 'packages', 'renderer-worker', 'dist', 'rendererWorkerMain.js')
+
+const content = await readFile(rendererWorkerMainPath, 'utf-8')
+
+const remoteUrl = getRemoteUrl(workerPath)
+if (!content.includes('// const extensionHostWorkerUrl = ')) {
+  const occurrence = `const extensionHostWorkerUrl = \`\${assetDir}/packages/extension-host-worker/dist/extensionHostWorkerMain.js\``
+  const replacement = `// const extensionHostWorkerUrl = \`\${assetDir}/packages/extension-host-worker/dist/extensionHostWorkerMain.js\`
+const extensionHostWorkerUrl = \`${remoteUrl}\``
+
+  const newContent = content.replace(occurrence, replacement)
+  await writeFile(rendererWorkerMainPath, newContent)
+}
