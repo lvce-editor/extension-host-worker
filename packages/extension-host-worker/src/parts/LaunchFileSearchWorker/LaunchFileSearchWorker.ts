435e839b8ff26fbb67d158f126344aaf7c1e661f
Le Vivilet <levivilet@pm.me>
2026-05-29 12:04:41 +0200

    refactor: remove error handling from launchFileSearchWorker function (#566)
    * refactor: remove error handling from launchFileSearchWorker function
    
    * feat: add test path argument to server execution in dev script

--- packages/extension-host-worker/src/parts/LaunchFileSearchWorker/LaunchFileSearchWorker.ts
+++ packages/extension-host-worker/src/parts/LaunchFileSearchWorker/LaunchFileSearchWorker.ts
@@ -2,15 +2,11 @@ import { LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
 import { FileSearchWorker, RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const launchFileSearchWorker = async (): Promise<void> => {
-  try {
-    const rpc = await LazyTransferMessagePortRpcParent.create({
-      commandMap: {},
-      async send(port) {
-        await RendererWorker.sendMessagePortToFileSearchWorker(port, 0)
-      },
-    })
-    FileSearchWorker.set(rpc)
-  } catch {
-    // ignore
-  }
+  const rpc = await LazyTransferMessagePortRpcParent.create({
+    commandMap: {},
+    async send(port) {
+      await RendererWorker.sendMessagePortToFileSearchWorker(port, 0)
+    },
+  })
+  FileSearchWorker.set(rpc)
 }
