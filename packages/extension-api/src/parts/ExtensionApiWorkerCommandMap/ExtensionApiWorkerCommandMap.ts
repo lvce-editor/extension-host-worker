e3fb04363ca3b161991c88df628668bacfc716a7
Le Vivilet <levivilet@pm.me>
2026-06-01 19:20:51 +0200

    api

--- packages/extension-api/src/parts/ExtensionApiWorkerCommandMap/ExtensionApiWorkerCommandMap.ts
+++ packages/extension-api/src/parts/ExtensionApiWorkerCommandMap/ExtensionApiWorkerCommandMap.ts
@@ -1,11 +1,7 @@
-import { activateExtension } from '../ActivateExtension/ActivateExtension.ts'
 import { commandMap as extensionApiCommandMap } from '../CommandMap/CommandMap.ts'
 import { handleExtensionManagementMessagePort } from '../ExtensionApiWorkerHandleMessagePort/ExtensionApiWorkerHandleMessagePort.ts'
-import { importExtension } from '../ImportExtension/ImportExtension.ts'
 
 export const commandMap = {
   ...extensionApiCommandMap,
-  'ExtensionHost.activateExtension3': activateExtension,
-  'ExtensionHost.importExtension2': importExtension,
   'HandleMessagePort.handleExtensionManagementMessagePort': handleExtensionManagementMessagePort,
 }
--- packages/extension-api/src/parts/ImportExtension/ImportExtension.ts
+++
@@ -1,6 +0,0 @@
-import * as ExtensionModules from '../ExtensionModules/ExtensionModules.ts'
-
-export const importExtension = async (extensionId: string, absolutePath: string): Promise<void> => {
-  const module = await import(absolutePath)
-  ExtensionModules.set(extensionId, module)
-}
