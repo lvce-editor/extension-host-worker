5d29863c1ad32b169ea0ec461cd74aacc5046708
Le Vivilet <levivilet@pm.me>
2026-06-01 22:56:43 +0200

    fix

--- packages/extension-api/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-api/src/parts/CommandMap/CommandMap.ts
@@ -2,4 +2,7 @@ import { getStatusBarItems } from '../GetStatusBarItems/GetStatusBarItems.ts'
 
 export const commandMap = {
   'ExtensionApi.getStatusBarItems': getStatusBarItems,
+  initialize() {
+    // TODO
+  },
 }
