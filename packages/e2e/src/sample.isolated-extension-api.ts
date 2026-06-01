9b78dcff71834400fdbd8c278e4f42d6bcee5d01
Le Vivilet <levivilet@pm.me>
2026-06-01 16:38:34 +0200

    e2e

--- packages/e2e/src/sample.isolated-extension-api.ts
+++ packages/e2e/src/sample.isolated-extension-api.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'sample.isolated-extension-api'
 
+export const skip = 1
+
 export const test: Test = async ({ Extension, Locator, expect }) => {
   const uri = import.meta.resolve(`../fixtures/${name}`)
   await Extension.addWebExtension(uri)
