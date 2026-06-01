9944e06d4619a63ae734803b125e51f48968ea09
Le Vivilet <levivilet@pm.me>
2026-06-02 00:08:45 +0200

    status bar api

--- packages/extension-api/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-api/src/parts/CommandMap/CommandMap.ts
@@ -2,7 +2,13 @@ import { getStatusBarItems } from '../GetStatusBarItems/GetStatusBarItems.ts'
 
 export const commandMap = {
   'ExtensionApi.getStatusBarItems': getStatusBarItems,
-  initialize() {
-    // TODO
+  async initialize(type: string, port: MessagePort): Promise<void> {
+    if (type !== 'message-port') {
+      throw new Error(`unsupported initialize type ${type}`)
+    }
+    const { handleExtensionManagementMessagePort } = await import(
+      '../ExtensionApiWorkerHandleMessagePort/ExtensionApiWorkerHandleMessagePort.ts'
+    )
+    await handleExtensionManagementMessagePort(port)
   },
 }
