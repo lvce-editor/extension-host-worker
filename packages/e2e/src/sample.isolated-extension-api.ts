ed8bb776e133a5c5827214141e33c2948cebe158
Le Vivilet <levivilet@pm.me>
2026-06-02 16:39:54 +0200

    remove skip flag from isolated extension API and formatting provider tests (#589)

--- packages/e2e/src/sample.isolated-extension-api.ts
+++ packages/e2e/src/sample.isolated-extension-api.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'sample.isolated-extension-api'
 
-export const skip = true
-
 export const test: Test = async ({ expect, Extension, Locator }) => {
   const uri = import.meta.resolve(`../fixtures/${name}`)
   await Extension.addWebExtension(uri)
