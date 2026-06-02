b1d32c453d902cd6c34b7d9fd4d713f6f4e96e9a
Le Vivilet <levivilet@pm.me>
2026-06-02 16:37:52 +0200

    remove skip flag from isolated extension API and formatting provider tests

--- packages/e2e/src/sample.webview-dynamic.ts
+++ packages/e2e/src/sample.webview-dynamic.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'sample.webview-dynamic'
 
-export const skip = true
-
 export const test: Test = async ({ Extension, Main }) => {
   // TODO open sample webview
   // TODO verify that expected content is displayed
