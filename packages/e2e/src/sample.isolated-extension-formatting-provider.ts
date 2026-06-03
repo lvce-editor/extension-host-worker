98efee0b35e271764727e43831bc3306b941cc7c
Le Vivilet <levivilet@pm.me>
2026-06-03 18:12:11 +0200

    feature: remove unused Command parameter and improve test structure in isolated extension formatting provider (#597)

--- packages/e2e/src/sample.isolated-extension-formatting-provider.ts
+++ packages/e2e/src/sample.isolated-extension-formatting-provider.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'sample.isolated-extension-formatting-provider'
 
-export const test: Test = async ({ Command, Editor, Extension, FileSystem, Main, Workspace }) => {
+export const test: Test = async ({ Editor, Extension, FileSystem, Main, Workspace }) => {
   const uri = import.meta.resolve(`../fixtures/${name}`)
   await Extension.addWebExtension(uri)
   const tmpDir = await FileSystem.getTmpDir()
@@ -10,7 +10,9 @@ export const test: Test = async ({ Command, Editor, Extension, FileSystem, Main,
   await Workspace.setPath(tmpDir)
   await Main.openUri(`${tmpDir}/test.js`)
 
-  await Command.execute('Editor.format')
+  // act
+  await Editor.format()
 
+  // assert
   await Editor.shouldHaveText('const value = 1')
 }
