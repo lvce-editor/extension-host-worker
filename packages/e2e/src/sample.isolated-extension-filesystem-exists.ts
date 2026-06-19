49da8036438251ecb1e751a649709d44ac711e90
Le Vivilet <levivilet@pm.me>
2026-06-19 14:38:17 +0200

    skip failing filesystem e2e tests

--- packages/e2e/src/sample.isolated-extension-filesystem-exists.ts
+++ packages/e2e/src/sample.isolated-extension-filesystem-exists.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'sample.isolated-extension-filesystem-exists'
 
+export const skip = true
+
 export const test: Test = async ({ Command, Extension }) => {
   const uri = import.meta.resolve(`../fixtures/${name}`)
   await Extension.addWebExtension(uri)
--- packages/e2e/src/sample.isolated-extension-filesystem-mkdir.ts
+++ packages/e2e/src/sample.isolated-extension-filesystem-mkdir.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'sample.isolated-extension-filesystem-mkdir'
 
+export const skip = true
+
 export const test: Test = async ({ Command, Extension, FileSystem }) => {
   const uri = import.meta.resolve(`../fixtures/${name}`)
   const tmpDir = await FileSystem.getTmpDir()
