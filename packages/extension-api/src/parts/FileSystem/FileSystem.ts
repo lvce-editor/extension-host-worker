38ece59676fd7830e1d78aa9eb750a69365d06b8
Le Vivilet <levivilet@pm.me>
2026-06-19 23:22:06 +0200

    feature: enhance FileSystem API to support memfs read operations and update tests

--- packages/extension-api/src/parts/FileSystem/FileSystem.ts
+++ packages/extension-api/src/parts/FileSystem/FileSystem.ts
@@ -1,5 +1,11 @@
-import { FileSystemWorker } from '@lvce-editor/rpc-registry'
+import { ExtensionManagementWorker, FileSystemWorker } from '@lvce-editor/rpc-registry'
 import type { FileSystemDirent } from '../FileSystemDirent/FileSystemDirent.ts'
+
+const MemfsPrefix = 'memfs://'
+
+const isMemory = (uri: string): boolean => {
+  return uri.startsWith(MemfsPrefix)
+}
 
 export const exists = async (uri: string): Promise<boolean> => {
   return FileSystemWorker.exists(uri)
@@ -10,6 +16,9 @@ export const readDirWithFileTypes = async (uri: string): Promise<readonly FileSy
 }
 
 export const readFile = async (uri: string): Promise<string> => {
+  if (isMemory(uri)) {
+    return ExtensionManagementWorker.invoke('ExtensionApi.readFile', uri)
+  }
   return FileSystemWorker.readFile(uri)
 }
 
