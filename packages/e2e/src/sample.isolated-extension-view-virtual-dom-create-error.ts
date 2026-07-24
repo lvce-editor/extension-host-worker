c376b75ad38ee2c6f5343b39365cdc1aa48628b4
Le Vivilet <levivilet@pm.me>
2026-07-24 12:09:42 +0200

    fix: verify view load error details (#668)

--- packages/e2e/src/sample.isolated-extension-view-virtual-dom-create-error.ts
+++ packages/e2e/src/sample.isolated-extension-view-virtual-dom-create-error.ts
@@ -9,6 +9,9 @@ export const test: Test = async ({ ActivityBar, expect, Extension, Locator }) =>
   await Extension.addWebExtension(uri)
   await ActivityBar.toggleActivityBarItem('sample.views.virtualDomCreateError')
 
-  const errorMessage = Locator('text=create failed')
-  await expect(errorMessage).toBeVisible()
+  const error = Locator('.Viewlet.Error')
+  await expect(error).toBeVisible()
+  await expect(error).toContainText('Error: create failed')
+  await expect(error).toContainText("throw new Error('create failed')")
+  await expect(error).toContainText('at create (')
 }
