7526b7c4c148554d98d47a6c882470d7f7079ba3
Le Vivilet <levivilet@pm.me>
2026-07-25 23:09:38 +0000

    feature: add extension notification api

--- packages/extension-api/src/parts/Host/Host.ts
+++ packages/extension-api/src/parts/Host/Host.ts
@@ -1,5 +1,7 @@
 import { executeCommand } from '../ExecuteCommand/ExecuteCommand.ts'
 
+export type NotificationType = 'error' | 'info' | 'warning'
+
 export const confirm = async (message: string): Promise<boolean> => {
   return Boolean(await executeCommand('ConfirmPrompt.prompt', message))
 }
@@ -18,4 +20,8 @@ export const handleWorkspaceRefresh = async (): Promise<void> => {
 
 export const openUri = async (uri: string): Promise<void> => {
   await executeCommand('Main.openUri', uri)
+}
+
+export const showNotification = async (type: NotificationType, message: string): Promise<void> => {
+  await executeCommand('Notification.create', type, message)
 }
