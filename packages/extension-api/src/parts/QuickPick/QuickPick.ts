646562477f502533e224ba0dcf725ed29d2dc97e
Le Vivilet <levivilet@pm.me>
2026-08-08 20:46:51 +0000

    feature: expose file quick pick api

--- packages/extension-api/src/parts/QuickPick/QuickPick.ts
+++ packages/extension-api/src/parts/QuickPick/QuickPick.ts
@@ -1,6 +1,11 @@
 import { ExtensionManagementWorker } from '@lvce-editor/rpc-registry'
+import { executeCommand } from '../ExecuteCommand/ExecuteCommand.ts'
 import type { ShowQuickInputOptions } from '../ShowQuickInputOptions/ShowQuickInputOptions.ts'
 import type { ShowQuickPickOptions } from '../ShowQuickPickOptions/ShowQuickPickOptions.ts'
+
+export const showFileQuickPick = async (): Promise<void> => {
+  await executeCommand('QuickPick.showFile')
+}
 
 export const showQuickInput = async (options: ShowQuickInputOptions = {}): Promise<string | undefined> => {
   return ExtensionManagementWorker.invoke('ExtensionHostQuickPick.showQuickInput', options) as Promise<string | undefined>
