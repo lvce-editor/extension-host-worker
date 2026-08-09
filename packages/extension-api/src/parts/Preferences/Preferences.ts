487bb999c4e2de6799d3820ab8ad3151bff2636f
Le Vivilet <levivilet@pm.me>
2026-08-09 08:46:01 +0000

    feature: expose open settings api

--- packages/extension-api/src/parts/Preferences/Preferences.ts
+++ packages/extension-api/src/parts/Preferences/Preferences.ts
@@ -1,7 +1,12 @@
 import { ExtensionManagementWorker } from '@lvce-editor/rpc-registry'
+import { executeCommand } from '../ExecuteCommand/ExecuteCommand.ts'
 
 export const getPreference = async (key: string): Promise<unknown> => {
   return ExtensionManagementWorker.invoke('Extensions.getPreference', key)
+}
+
+export const openSettings = async (): Promise<void> => {
+  await executeCommand('Preferences.openSettingsUi')
 }
 
 export const setPreference = async (key: string, value: unknown): Promise<void> => {
